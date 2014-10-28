36b133bf91f71639db00bc5c6d659e74c94b722f
Pokechu22 <pokechu022@gmail.com>
2014-10-28 14:18:49 -0700

    Get the set values.

--- GBTD_GBMB/GBCOMP/ChoosePalette.cs
+++ GBTD_GBMB/GBCOMP/ChoosePalette.cs
@@ -11,6 +11,42 @@ namespace GBRenderer
 {
 	public partial class ChoosePalette : Form
 	{
+		/// <summary>
+		/// The whitemost color.
+		/// </summary>
+		public Color WhiteColor {
+			get {
+				return gbPaletteSetSelector1.Colors[0, 0];
+			}
+		}
+
+		/// <summary>
+		/// The light-gray color.
+		/// </summary>
+		public Color LightGrayColor {
+			get {
+				return gbPaletteSetSelector1.Colors[1, 0];
+			}
+		}
+
+		/// <summary>
+		/// The dark-gray color.
+		/// </summary>
+		public Color DarkGrayColor {
+			get {
+				return gbPaletteSetSelector1.Colors[2, 0];
+			}
+		}
+
+		/// <summary>
+		/// The black color.
+		/// </summary>
+		public Color BlackColor {
+			get {
+				return gbPaletteSetSelector1.Colors[3, 0];
+			}
+		}
+
 		public ChoosePalette() {
 			InitializeComponent();
 			
