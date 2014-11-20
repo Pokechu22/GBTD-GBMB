13c9d348dcaaae15619c770a7c2df295d46a2ce5
Pokechu22 <pokechu022@gmail.com>
2014-11-20 10:12:39 -0800

    Preview in editor.

--- GBTD_GBMB/GBPalette/PaletteObjectsEditor.cs
+++ GBTD_GBMB/GBPalette/PaletteObjectsEditor.cs
@@ -28,16 +28,42 @@ namespace GB.Shared.Palette
 			}
 
 			if (editorService != null) {
-				TForm form = new TForm();
+				TSet set = value as TSet;
+				if (set != null) {
+					TForm form = new TForm();
 
-				form.Set = (TSet)value;
+					form.Set = (TSet)value;
 
-				editorService.ShowDialog(form);
+					editorService.ShowDialog(form);
 
-				value = form.Set;
+					value = form.Set;
+				}
 			}
 
 			return value;
+		}
+
+		public override void PaintValue(PaintValueEventArgs e) {
+			TSet set = e.Value as TSet;
+			if (set != null) {
+				e.Graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor;
+				//Draws horizontally.
+				float width = (float)e.Bounds.Width / 4f;
+				float height = (float)e.Bounds.Height / (float)set.NumberOfRows;
+
+				for (int x = 0; x < 4; x++) {
+					for (int y = 0; y < set.NumberOfRows; y++) {
+						using (Brush b = new SolidBrush(set[y][x])) {
+							e.Graphics.FillRectangle(b, e.Bounds.X + (x * width), e.Bounds.Y + (y * height), width, height);
+							//e.Graphics.DrawRectangle(Pens.Black, e.Bounds.X + (x * width), e.Bounds.Y + (y * height), width, height);
+						}
+					}
+				}
+			}
+		}
+
+		public override bool GetPaintValueSupported(ITypeDescriptorContext context) {
+			return true;
 		}
 	}
 }
