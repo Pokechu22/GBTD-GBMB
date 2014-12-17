d4e9017aa8ad8c288def564412123050d50dd13b
Pokechu22 <pokechu022@gmail.com>
2014-12-17 12:05:44 -0800

    Set up some extension methods to modify the new PaletteObjects.

--- GBTD_GBMB/GBPalette/PaletteObjects_.cs
+++ GBTD_GBMB/GBPalette/PaletteObjects_.cs
@@ -10,7 +10,6 @@ namespace GB.Shared.Palette
 	public interface IPaletteSetBehavior
 	{
 		IPaletteEntryBehavior EntryBehavior { get; }
-		int Width { get; }
 		int Height { get; }
 	}
 
@@ -31,6 +30,11 @@ namespace GB.Shared.Palette
 	{
 		public readonly Palette_[] palettes;
 		public readonly IPaletteSetBehavior behaviour;
+
+		public PaletteSet_(Palette_[] palettes, IPaletteSetBehavior behaviour) {
+			this.palettes = palettes;
+			this.behaviour = behaviour;
+		}
 	}
 
 	public struct Palette_
@@ -51,6 +55,13 @@ namespace GB.Shared.Palette
 				}
 			}
 		}
+
+		public Palette_(PaletteEntry_ entry0, PaletteEntry_ entry1, PaletteEntry_ entry2, PaletteEntry_ entry3) {
+			this.entry0 = entry0;
+			this.entry1 = entry1;
+			this.entry2 = entry2;
+			this.entry3 = entry3;
+		}
 	}
 
 	public struct PaletteEntry_
@@ -59,16 +70,70 @@ namespace GB.Shared.Palette
 		public readonly int x;
 		public readonly Color color;
 
-		public readonly IPaletteEntryBehavior transform;
+		public readonly IPaletteEntryBehavior behavior;
 
 		public Color DisplayColor {
 			get {
-				if (transform != null) {
-					return transform.GetFilteredColor(this);
+				if (behavior != null) {
+					return behavior.GetFilteredColor(this);
 				} else {
 					return this.color;
 				}
 			}
+		}
+
+		public PaletteEntry_(int x, int y, Color color, IPaletteEntryBehavior behavior) {
+			this.x = x;
+			this.y = y;
+			this.color = color;
+			this.behavior = behavior;
+		}
+	}
+
+	/// <summary>
+	/// Contains the extension methods needed.
+	/// </summary>
+	public static class PaletteExtensions
+	{
+		public static PaletteSet_ SetEntryColor(this PaletteSet_ @this, int x, int y, Color color) {
+			if (x < 0 || x >= 4) {
+				throw new ArgumentOutOfRangeException("x", x, "Must be in range of 0 ≤ x < 4 (the width)");
+			}
+			if (y < 0 || y >= @this.behaviour.Height) {
+				throw new ArgumentOutOfRangeException("y", y, "Must be in range of 0 ≤ y < " + @this.behaviour.Height + " (the height)");
+			}
+
+			Palette_[] palettes = @this.palettes.SetEntryColor(x, y, color);
+
+			return new PaletteSet_(palettes, @this.behaviour);
+		}
+
+		public static Palette_[] SetEntryColor(this Palette_[] @this, int x, int y, Color color) {
+			if (x < 0 || x >= 4) {
+				throw new ArgumentOutOfRangeException("x", x, "Must be in range of 0 ≤ x < 4 (the width)");
+			}
+			if (y < 0 || y >= @this.Length) {
+				throw new ArgumentOutOfRangeException("y", y, "Must be in range of 0 ≤ y < " + @this.Length + " (the height)");
+			}
+
+			Palette_[] palettes = (Palette_[])@this.Clone();
+			palettes[y] = @this[y].SetEntryColor(x, color);
+
+			return palettes;
+		}
+
+		public static Palette_ SetEntryColor(this Palette_ @this, int x, Color color) {
+			switch (x) {
+			case 0: return new Palette_(@this.entry0.SetColor(color), @this.entry1, @this.entry2, @this.entry3);
+			case 1: return new Palette_(@this.entry0, @this.entry1.SetColor(color), @this.entry2, @this.entry3);
+			case 2: return new Palette_(@this.entry0, @this.entry1, @this.entry2.SetColor(color), @this.entry3);
+			case 3: return new Palette_(@this.entry0, @this.entry1, @this.entry2, @this.entry3.SetColor(color));
+			}
+			throw new ArgumentOutOfRangeException("x", x, "Must be in range of 0 ≤ x < 4 (the width)");
+		}
+
+		public static PaletteEntry_ SetColor(this PaletteEntry_ @this, Color color) {
+			return new PaletteEntry_(@this.x, @this.y, color, @this.behavior);
 		}
 	}
 }
