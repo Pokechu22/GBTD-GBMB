21de84f25af2a4bad7a0c4eaa8e7d9dbafa11a31
Pokechu22 <pokechu022@gmail.com>
2015-01-13 09:40:28 -0800

    Apparently I didn't add this yet.

---
+++ GBTD_GBMB/GBLib/Tiles/TileData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+using GB.Shared.Palettes;
+
+namespace GB.Shared.Tiles
+{
+	public struct TileData
+	{
+		public PaletteSet set;
+		public int paletteID;
+		public Tile tile;
+
+		/// <summary>
+		/// The palette used for this tileData.
+		/// </summary>
+		public Palette Palette {
+			get { return set.Rows[paletteID]; }
+		}
+	}
+}
