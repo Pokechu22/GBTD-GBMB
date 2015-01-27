ec63dff994253c70295ca185628a3c0389108f16
Pokechu22 <pokechu022@gmail.com>
2015-01-27 11:57:03 -0800

    Added the general controls needed for the Settings section.
    Possitions aren't perfect yet.

--- GBTD_GBMB/GBTD/Dialogs/ExportDialog.Designer.cs
+++ GBTD_GBMB/GBTD/Dialogs/ExportDialog.Designer.cs
@@ -25,8 +25,15 @@
 		/// the contents of this method with the code editor.
 		/// </summary>
 		private void InitializeComponent() {
-			System.Drawing.StringFormat stringFormat2 = new System.Drawing.StringFormat();
+			System.Drawing.StringFormat stringFormat9 = new System.Drawing.StringFormat();
+			System.Drawing.StringFormat stringFormat8 = new System.Drawing.StringFormat();
 			System.Drawing.StringFormat stringFormat1 = new System.Drawing.StringFormat();
+			System.Drawing.StringFormat stringFormat2 = new System.Drawing.StringFormat();
+			System.Drawing.StringFormat stringFormat3 = new System.Drawing.StringFormat();
+			System.Drawing.StringFormat stringFormat4 = new System.Drawing.StringFormat();
+			System.Drawing.StringFormat stringFormat5 = new System.Drawing.StringFormat();
+			System.Drawing.StringFormat stringFormat6 = new System.Drawing.StringFormat();
+			System.Drawing.StringFormat stringFormat7 = new System.Drawing.StringFormat();
 			this.okButton = new System.Windows.Forms.Button();
 			this.helpButton = new System.Windows.Forms.Button();
 			this.cancelButton = new System.Windows.Forms.Button();
@@ -34,17 +41,34 @@
 			this.tabPageStandard = new System.Windows.Forms.TabPage();
 			this.groupBoxSettings = new GB.Shared.Controls.GroupBox();
 			this.groupBoxFile = new GB.Shared.Controls.GroupBox();
-			this.fileTypeComboBox = new System.Windows.Forms.ComboBox();
-			this.labelFileName = new GB.Shared.Controls.CleanLabel();
-			this.labelType = new GB.Shared.Controls.CleanLabel();
-			this.browseButton = new System.Windows.Forms.Button();
 			this.fileNameTextBox = new System.Windows.Forms.TextBox();
+			this.browseButton = new System.Windows.Forms.Button();
+			this.labelType = new GB.Shared.Controls.CleanLabel();
+			this.labelFileName = new GB.Shared.Controls.CleanLabel();
+			this.fileTypeComboBox = new System.Windows.Forms.ComboBox();
 			this.tabPageAdvanced = new System.Windows.Forms.TabPage();
 			this.groupBoxSplitData = new GB.Shared.Controls.GroupBox();
 			this.groupBoxMetatiles = new GB.Shared.Controls.GroupBox();
 			this.groupBoxColors = new GB.Shared.Controls.GroupBox();
+			this.labelLabel = new GB.Shared.Controls.CleanLabel();
+			this.labelSection = new GB.Shared.Controls.CleanLabel();
+			this.labelBank = new GB.Shared.Controls.CleanLabel();
+			this.labelFrom = new GB.Shared.Controls.CleanLabel();
+			this.labelTo = new GB.Shared.Controls.CleanLabel();
+			this.labelFormat = new GB.Shared.Controls.CleanLabel();
+			this.labelCounter = new GB.Shared.Controls.CleanLabel();
+			this.labelTextBox = new System.Windows.Forms.TextBox();
+			this.sectionTextBox = new System.Windows.Forms.TextBox();
+			this.bankTextBox = new System.Windows.Forms.TextBox();
+			this.fromTextBox = new System.Windows.Forms.TextBox();
+			this.toTextBox = new System.Windows.Forms.TextBox();
+			this.formatComboBox = new System.Windows.Forms.ComboBox();
+			this.counterComboBox = new System.Windows.Forms.ComboBox();
+			this.singleUnitCheckBox = new System.Windows.Forms.CheckBox();
+			this.gbCompresCheckBox = new System.Windows.Forms.CheckBox();
 			this.tabControl.SuspendLayout();
 			this.tabPageStandard.SuspendLayout();
+			this.groupBoxSettings.SuspendLayout();
 			this.groupBoxFile.SuspendLayout();
 			this.tabPageAdvanced.SuspendLayout();
 			this.SuspendLayout();
@@ -101,6 +125,22 @@
 			// 
 			// groupBoxSettings
 			// 
+			this.groupBoxSettings.Controls.Add(this.gbCompresCheckBox);
+			this.groupBoxSettings.Controls.Add(this.singleUnitCheckBox);
+			this.groupBoxSettings.Controls.Add(this.counterComboBox);
+			this.groupBoxSettings.Controls.Add(this.labelLabel);
+			this.groupBoxSettings.Controls.Add(this.labelSection);
+			this.groupBoxSettings.Controls.Add(this.labelBank);
+			this.groupBoxSettings.Controls.Add(this.labelFrom);
+			this.groupBoxSettings.Controls.Add(this.labelTo);
+			this.groupBoxSettings.Controls.Add(this.labelFormat);
+			this.groupBoxSettings.Controls.Add(this.labelCounter);
+			this.groupBoxSettings.Controls.Add(this.labelTextBox);
+			this.groupBoxSettings.Controls.Add(this.sectionTextBox);
+			this.groupBoxSettings.Controls.Add(this.bankTextBox);
+			this.groupBoxSettings.Controls.Add(this.fromTextBox);
+			this.groupBoxSettings.Controls.Add(this.toTextBox);
+			this.groupBoxSettings.Controls.Add(this.formatComboBox);
 			this.groupBoxSettings.Location = new System.Drawing.Point(8, 90);
 			this.groupBoxSettings.Margin = new System.Windows.Forms.Padding(3, 4, 3, 3);
 			this.groupBoxSettings.Name = "groupBoxSettings";
@@ -110,16 +150,58 @@
 			// 
 			// groupBoxFile
 			// 
-			this.groupBoxFile.Controls.Add(this.fileNameTextBox);
-			this.groupBoxFile.Controls.Add(this.browseButton);
-			this.groupBoxFile.Controls.Add(this.labelType);
-			this.groupBoxFile.Controls.Add(this.labelFileName);
 			this.groupBoxFile.Controls.Add(this.fileTypeComboBox);
+			this.groupBoxFile.Controls.Add(this.labelFileName);
+			this.groupBoxFile.Controls.Add(this.labelType);
+			this.groupBoxFile.Controls.Add(this.browseButton);
+			this.groupBoxFile.Controls.Add(this.fileNameTextBox);
 			this.groupBoxFile.Location = new System.Drawing.Point(8, 10);
 			this.groupBoxFile.Name = "groupBoxFile";
 			this.groupBoxFile.Size = new System.Drawing.Size(433, 73);
 			this.groupBoxFile.TabIndex = 0;
 			this.groupBoxFile.Text = "File";
+			// 
+			// fileNameTextBox
+			// 
+			this.fileNameTextBox.Location = new System.Drawing.Point(72, 16);
+			this.fileNameTextBox.Name = "fileNameTextBox";
+			this.fileNameTextBox.Size = new System.Drawing.Size(281, 20);
+			this.fileNameTextBox.TabIndex = 7;
+			// 
+			// browseButton
+			// 
+			this.browseButton.Location = new System.Drawing.Point(360, 16);
+			this.browseButton.Name = "browseButton";
+			this.browseButton.Size = new System.Drawing.Size(65, 21);
+			this.browseButton.TabIndex = 6;
+			this.browseButton.Text = "&Browse...";
+			this.browseButton.UseVisualStyleBackColor = true;
+			// 
+			// labelType
+			// 
+			stringFormat9.Alignment = System.Drawing.StringAlignment.Near;
+			stringFormat9.HotkeyPrefix = System.Drawing.Text.HotkeyPrefix.Show;
+			stringFormat9.LineAlignment = System.Drawing.StringAlignment.Near;
+			stringFormat9.Trimming = System.Drawing.StringTrimming.Character;
+			this.labelType.Format = stringFormat9;
+			this.labelType.Location = new System.Drawing.Point(14, 43);
+			this.labelType.Name = "labelType";
+			this.labelType.Size = new System.Drawing.Size(29, 14);
+			this.labelType.TabIndex = 5;
+			this.labelType.Text = "T&ype";
+			// 
+			// labelFileName
+			// 
+			stringFormat8.Alignment = System.Drawing.StringAlignment.Near;
+			stringFormat8.HotkeyPrefix = System.Drawing.Text.HotkeyPrefix.Show;
+			stringFormat8.LineAlignment = System.Drawing.StringAlignment.Near;
+			stringFormat8.Trimming = System.Drawing.StringTrimming.Character;
+			this.labelFileName.Format = stringFormat8;
+			this.labelFileName.Location = new System.Drawing.Point(14, 19);
+			this.labelFileName.Name = "labelFileName";
+			this.labelFileName.Size = new System.Drawing.Size(51, 14);
+			this.labelFileName.TabIndex = 4;
+			this.labelFileName.Text = "Filena&me";
 			// 
 			// fileTypeComboBox
 			// 
@@ -136,48 +218,6 @@
 			this.fileTypeComboBox.Name = "fileTypeComboBox";
 			this.fileTypeComboBox.Size = new System.Drawing.Size(169, 21);
 			this.fileTypeComboBox.TabIndex = 8;
-			// 
-			// labelFileName
-			// 
-			stringFormat2.Alignment = System.Drawing.StringAlignment.Near;
-			stringFormat2.HotkeyPrefix = System.Drawing.Text.HotkeyPrefix.Show;
-			stringFormat2.LineAlignment = System.Drawing.StringAlignment.Near;
-			stringFormat2.Trimming = System.Drawing.StringTrimming.Character;
-			this.labelFileName.Format = stringFormat2;
-			this.labelFileName.Location = new System.Drawing.Point(14, 19);
-			this.labelFileName.Name = "labelFileName";
-			this.labelFileName.Size = new System.Drawing.Size(51, 14);
-			this.labelFileName.TabIndex = 4;
-			this.labelFileName.Text = "Filena&me";
-			// 
-			// labelType
-			// 
-			stringFormat1.Alignment = System.Drawing.StringAlignment.Near;
-			stringFormat1.HotkeyPrefix = System.Drawing.Text.HotkeyPrefix.Show;
-			stringFormat1.LineAlignment = System.Drawing.StringAlignment.Near;
-			stringFormat1.Trimming = System.Drawing.StringTrimming.Character;
-			this.labelType.Format = stringFormat1;
-			this.labelType.Location = new System.Drawing.Point(14, 43);
-			this.labelType.Name = "labelType";
-			this.labelType.Size = new System.Drawing.Size(29, 14);
-			this.labelType.TabIndex = 5;
-			this.labelType.Text = "T&ype";
-			// 
-			// browseButton
-			// 
-			this.browseButton.Location = new System.Drawing.Point(360, 16);
-			this.browseButton.Name = "browseButton";
-			this.browseButton.Size = new System.Drawing.Size(65, 21);
-			this.browseButton.TabIndex = 6;
-			this.browseButton.Text = "&Browse...";
-			this.browseButton.UseVisualStyleBackColor = true;
-			// 
-			// fileNameTextBox
-			// 
-			this.fileNameTextBox.Location = new System.Drawing.Point(72, 16);
-			this.fileNameTextBox.Name = "fileNameTextBox";
-			this.fileNameTextBox.Size = new System.Drawing.Size(281, 20);
-			this.fileNameTextBox.TabIndex = 7;
 			// 
 			// tabPageAdvanced
 			// 
@@ -218,6 +258,171 @@
 			this.groupBoxColors.TabIndex = 0;
 			this.groupBoxColors.Text = "Colors";
 			// 
+			// labelLabel
+			// 
+			stringFormat1.Alignment = System.Drawing.StringAlignment.Near;
+			stringFormat1.HotkeyPrefix = System.Drawing.Text.HotkeyPrefix.Show;
+			stringFormat1.LineAlignment = System.Drawing.StringAlignment.Near;
+			stringFormat1.Trimming = System.Drawing.StringTrimming.Character;
+			this.labelLabel.Format = stringFormat1;
+			this.labelLabel.Location = new System.Drawing.Point(14, 16);
+			this.labelLabel.Name = "labelLabel";
+			this.labelLabel.Size = new System.Drawing.Size(32, 14);
+			this.labelLabel.TabIndex = 4;
+			this.labelLabel.Text = "&Label";
+			// 
+			// labelSection
+			// 
+			stringFormat2.Alignment = System.Drawing.StringAlignment.Near;
+			stringFormat2.HotkeyPrefix = System.Drawing.Text.HotkeyPrefix.Show;
+			stringFormat2.LineAlignment = System.Drawing.StringAlignment.Near;
+			stringFormat2.Trimming = System.Drawing.StringTrimming.Character;
+			this.labelSection.Format = stringFormat2;
+			this.labelSection.Location = new System.Drawing.Point(14, 42);
+			this.labelSection.Name = "labelSection";
+			this.labelSection.Size = new System.Drawing.Size(42, 14);
+			this.labelSection.TabIndex = 5;
+			this.labelSection.Text = "&Section";
+			// 
+			// labelBank
+			// 
+			stringFormat3.Alignment = System.Drawing.StringAlignment.Near;
+			stringFormat3.HotkeyPrefix = System.Drawing.Text.HotkeyPrefix.Show;
+			stringFormat3.LineAlignment = System.Drawing.StringAlignment.Near;
+			stringFormat3.Trimming = System.Drawing.StringTrimming.Character;
+			this.labelBank.Format = stringFormat3;
+			this.labelBank.Location = new System.Drawing.Point(14, 68);
+			this.labelBank.Name = "labelBank";
+			this.labelBank.Size = new System.Drawing.Size(30, 14);
+			this.labelBank.TabIndex = 6;
+			this.labelBank.Text = "&Bank";
+			// 
+			// labelFrom
+			// 
+			stringFormat4.Alignment = System.Drawing.StringAlignment.Near;
+			stringFormat4.HotkeyPrefix = System.Drawing.Text.HotkeyPrefix.Show;
+			stringFormat4.LineAlignment = System.Drawing.StringAlignment.Near;
+			stringFormat4.Trimming = System.Drawing.StringTrimming.Character;
+			this.labelFrom.Format = stringFormat4;
+			this.labelFrom.Location = new System.Drawing.Point(14, 100);
+			this.labelFrom.Name = "labelFrom";
+			this.labelFrom.Size = new System.Drawing.Size(31, 14);
+			this.labelFrom.TabIndex = 7;
+			this.labelFrom.Text = "&From";
+			// 
+			// labelTo
+			// 
+			stringFormat5.Alignment = System.Drawing.StringAlignment.Near;
+			stringFormat5.HotkeyPrefix = System.Drawing.Text.HotkeyPrefix.Show;
+			stringFormat5.LineAlignment = System.Drawing.StringAlignment.Near;
+			stringFormat5.Trimming = System.Drawing.StringTrimming.Character;
+			this.labelTo.Format = stringFormat5;
+			this.labelTo.Location = new System.Drawing.Point(129, 100);
+			this.labelTo.Name = "labelTo";
+			this.labelTo.Size = new System.Drawing.Size(17, 14);
+			this.labelTo.TabIndex = 8;
+			this.labelTo.Text = "&To";
+			// 
+			// labelFormat
+			// 
+			stringFormat6.Alignment = System.Drawing.StringAlignment.Near;
+			stringFormat6.HotkeyPrefix = System.Drawing.Text.HotkeyPrefix.Show;
+			stringFormat6.LineAlignment = System.Drawing.StringAlignment.Near;
+			stringFormat6.Trimming = System.Drawing.StringTrimming.Character;
+			this.labelFormat.Format = stringFormat6;
+			this.labelFormat.Location = new System.Drawing.Point(14, 131);
+			this.labelFormat.Name = "labelFormat";
+			this.labelFormat.Size = new System.Drawing.Size(40, 14);
+			this.labelFormat.TabIndex = 9;
+			this.labelFormat.Text = "F&ormat";
+			// 
+			// labelCounter
+			// 
+			stringFormat7.Alignment = System.Drawing.StringAlignment.Near;
+			stringFormat7.HotkeyPrefix = System.Drawing.Text.HotkeyPrefix.Show;
+			stringFormat7.LineAlignment = System.Drawing.StringAlignment.Near;
+			stringFormat7.Trimming = System.Drawing.StringTrimming.Character;
+			this.labelCounter.Format = stringFormat7;
+			this.labelCounter.Location = new System.Drawing.Point(14, 158);
+			this.labelCounter.Name = "labelCounter";
+			this.labelCounter.Size = new System.Drawing.Size(44, 14);
+			this.labelCounter.TabIndex = 10;
+			this.labelCounter.Text = "&Counter";
+			// 
+			// labelTextBox
+			// 
+			this.labelTextBox.Location = new System.Drawing.Point(72, 16);
+			this.labelTextBox.Name = "labelTextBox";
+			this.labelTextBox.Size = new System.Drawing.Size(100, 20);
+			this.labelTextBox.TabIndex = 11;
+			// 
+			// sectionTextBox
+			// 
+			this.sectionTextBox.Location = new System.Drawing.Point(72, 43);
+			this.sectionTextBox.Name = "sectionTextBox";
+			this.sectionTextBox.Size = new System.Drawing.Size(100, 20);
+			this.sectionTextBox.TabIndex = 12;
+			// 
+			// bankTextBox
+			// 
+			this.bankTextBox.Location = new System.Drawing.Point(72, 70);
+			this.bankTextBox.Name = "bankTextBox";
+			this.bankTextBox.Size = new System.Drawing.Size(100, 20);
+			this.bankTextBox.TabIndex = 13;
+			this.bankTextBox.Text = "0";
+			// 
+			// fromTextBox
+			// 
+			this.fromTextBox.Location = new System.Drawing.Point(72, 97);
+			this.fromTextBox.Name = "fromTextBox";
+			this.fromTextBox.Size = new System.Drawing.Size(38, 20);
+			this.fromTextBox.TabIndex = 14;
+			this.fromTextBox.Text = "0";
+			// 
+			// toTextBox
+			// 
+			this.toTextBox.Location = new System.Drawing.Point(149, 97);
+			this.toTextBox.Name = "toTextBox";
+			this.toTextBox.Size = new System.Drawing.Size(100, 20);
+			this.toTextBox.TabIndex = 15;
+			this.toTextBox.Text = "0";
+			// 
+			// formatComboBox
+			// 
+			this.formatComboBox.FormattingEnabled = true;
+			this.formatComboBox.Location = new System.Drawing.Point(72, 124);
+			this.formatComboBox.Name = "formatComboBox";
+			this.formatComboBox.Size = new System.Drawing.Size(121, 21);
+			this.formatComboBox.TabIndex = 16;
+			// 
+			// counterComboBox
+			// 
+			this.counterComboBox.FormattingEnabled = true;
+			this.counterComboBox.Location = new System.Drawing.Point(72, 151);
+			this.counterComboBox.Name = "counterComboBox";
+			this.counterComboBox.Size = new System.Drawing.Size(121, 21);
+			this.counterComboBox.TabIndex = 17;
+			// 
+			// singleUnitCheckBox
+			// 
+			this.singleUnitCheckBox.AutoSize = true;
+			this.singleUnitCheckBox.Location = new System.Drawing.Point(273, 13);
+			this.singleUnitCheckBox.Name = "singleUnitCheckBox";
+			this.singleUnitCheckBox.Size = new System.Drawing.Size(132, 17);
+			this.singleUnitCheckBox.TabIndex = 18;
+			this.singleUnitCheckBox.Text = "&Export tiles as one unit";
+			this.singleUnitCheckBox.UseVisualStyleBackColor = true;
+			// 
+			// gbCompresCheckBox
+			// 
+			this.gbCompresCheckBox.AutoSize = true;
+			this.gbCompresCheckBox.Location = new System.Drawing.Point(273, 42);
+			this.gbCompresCheckBox.Name = "gbCompresCheckBox";
+			this.gbCompresCheckBox.Size = new System.Drawing.Size(114, 17);
+			this.gbCompresCheckBox.TabIndex = 19;
+			this.gbCompresCheckBox.Text = "GB-Compre&ss data";
+			this.gbCompresCheckBox.UseVisualStyleBackColor = true;
+			// 
 			// ExportDialog
 			// 
 			this.AcceptButton = this.okButton;
@@ -236,6 +441,8 @@
 			this.Text = "Export";
 			this.tabControl.ResumeLayout(false);
 			this.tabPageStandard.ResumeLayout(false);
+			this.groupBoxSettings.ResumeLayout(false);
+			this.groupBoxSettings.PerformLayout();
 			this.groupBoxFile.ResumeLayout(false);
 			this.groupBoxFile.PerformLayout();
 			this.tabPageAdvanced.ResumeLayout(false);
@@ -261,5 +468,21 @@
 		private System.Windows.Forms.TextBox fileNameTextBox;
 		private System.Windows.Forms.Button browseButton;
 		private System.Windows.Forms.ComboBox fileTypeComboBox;
+		private System.Windows.Forms.TextBox labelTextBox;
+		private Shared.Controls.CleanLabel labelCounter;
+		private Shared.Controls.CleanLabel labelFormat;
+		private Shared.Controls.CleanLabel labelTo;
+		private Shared.Controls.CleanLabel labelFrom;
+		private Shared.Controls.CleanLabel labelBank;
+		private Shared.Controls.CleanLabel labelSection;
+		private Shared.Controls.CleanLabel labelLabel;
+		private System.Windows.Forms.TextBox sectionTextBox;
+		private System.Windows.Forms.ComboBox counterComboBox;
+		private System.Windows.Forms.TextBox bankTextBox;
+		private System.Windows.Forms.TextBox fromTextBox;
+		private System.Windows.Forms.TextBox toTextBox;
+		private System.Windows.Forms.ComboBox formatComboBox;
+		private System.Windows.Forms.CheckBox gbCompresCheckBox;
+		private System.Windows.Forms.CheckBox singleUnitCheckBox;
 	}
 }
