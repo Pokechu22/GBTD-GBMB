81271fc31647123a187991f9c7152cf8fa1d3b18
Pokechu22 <pokechu022@gmail.com>
2015-02-02 14:16:17 -0800

    Updated documentation and used the GBRObjectHeader.

--- GBTD_GBMB/GBRFile/IGBRExportable.cs
+++ GBTD_GBMB/GBRFile/IGBRExportable.cs
@@ -16,56 +16,24 @@ namespace GB.Shared.GBRFile
 		private static Dictionary<UInt16, GBRExportableContstructor> mapping = new Dictionary<ushort, GBRExportableContstructor>();
 
 		/// <summary>
-		/// The typeid of this object, which should remain constant.
+		/// The header of this object.
 		/// </summary>
-		public UInt16 TypeID { get; protected set; }
-
-		/// <summary>
-		/// The Unique ID of this object.
-		/// </summary>
-		public UInt16 UniqueID { get; protected set; }
-
-		/// <summary>
-		/// The size that this object was deserialized with.
-		/// </summary>
-		public UInt32 Size { get; protected set; }
+		protected GBRObjectHeader header;
 
 		protected IGBRExportable(UInt16 TypeID, UInt16 UniqueID, UInt32 Size, Stream stream) {
-			this.TypeID = TypeID;
-			this.UniqueID = UniqueID;
-			this.Size = Size;
+			this.header = new GBRObjectHeader(TypeID, UniqueID, Size);
+
+			//TODO
+		}
+
+		protected IGBRExportable(GBRObjectHeader header, Stream stream) {
+			this.header = header;
+
+			//TODO
 		}
 
 		public abstract void LoadFromStream(Stream s) {
 			
-		}
-
-		/// <summary>
-		/// Writes the object header to specified stream.
-		/// </summary>
-		/// <param name="stream">The stream to write to.</param>
-		protected void WriteHeader(Stream stream) {
-			if (!stream.CanWrite) {
-				throw new NotSupportedException("Stream cannot be written to.");
-			}
-
-			stream.WriteWord(TypeID);
-			stream.WriteWord(UniqueID);
-			stream.WriteLong(Size);
-		}
-
-		/// <summary>
-		/// Reads the object header from specified stream.
-		/// </summary>
-		/// <param name="stream">The stream to write to.</param>
-		protected void ReadHeader(Stream stream) {
-			if (!stream.CanRead) {
-				throw new NotSupportedException("Stream cannot be read from.");
-			}
-
-			this.TypeID = stream.ReadWord();
-			this.UniqueID = stream.ReadWord();
-			this.Size = Size;
 		}
 
 		public static IGBRExportable Read(Stream s) {
