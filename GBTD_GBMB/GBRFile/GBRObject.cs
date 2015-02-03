a3cb0305e91fd354e015eee750096664319312da
Pokechu22 <pokechu022@gmail.com>
2015-02-03 11:58:55 -0800

    Moved GBRObjectHeader to inside of GBRObject.cs.

--- GBTD_GBMB/GBRFile/GBRObject.cs
+++ GBTD_GBMB/GBRFile/GBRObject.cs
@@ -99,4 +99,32 @@ namespace GB.Shared.GBRFile
 			RegisterExportable(0xFF, typeof(GBRObjectDeleted));
 		}
 	}
+
+	public struct GBRObjectHeader
+	{
+		/// <summary>
+		/// The typeid of this object, which should remain constant.
+		/// </summary>
+		public readonly UInt16 ObjectID;
+
+		/// <summary>
+		/// The Unique ID of the object.
+		/// </summary>
+		public readonly UInt16 UniqueID;
+
+		/// <summary>
+		/// The size that this object was deserialized with.
+		/// </summary>
+		public readonly UInt32 Size;
+
+		public GBRObjectHeader(UInt16 ObjectID, UInt16 UniqueID, UInt32 Size) {
+			this.ObjectID = ObjectID;
+			this.UniqueID = UniqueID;
+			this.Size = Size;
+		}
+
+		public GBRObjectHeader Resize(UInt32 newSize) {
+			return new GBRObjectHeader(this.ObjectID, this.UniqueID, newSize);
+		}
+	}
 }
