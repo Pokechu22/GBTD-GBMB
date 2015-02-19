6aec73f6823fccc466166363797c05549efef281
Pokechu22 <pokechu022@gmail.com>
2015-02-19 08:11:35 -0800

    Moved the ColorSet enum to GBComp.

---
+++ GBTD_GBMB/GBLib/Palettes/ColorSet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GB.Shared.Palettes
+{
+	/// <summary>
+	/// Options that are valid for <see cref="GBRObjectTileSettings.ColorSet"/>.
+	/// </summary>
+	public enum ColorSet : byte
+	{
+		GAMEBOY_POCKET = 0,
+		GAMEBOY = 1,
+		GAMEBOY_COLOR = 2,
+		SUPER_GAMEBOY = 3,
+		GAMEBOY_COLOR_FILTERED = 4
+	}
+}
--- GBTD_GBMB/GBRFile/GBRObjectTileSettings.cs
+++ GBTD_GBMB/GBRFile/GBRObjectTileSettings.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.IO;
 using System.Windows.Forms;
 using GB.Shared.Tiles;
+using GB.Shared.Palettes;
 
 namespace GB.Shared.GBRFile
 {
@@ -246,17 +247,5 @@ namespace GB.Shared.GBRFile
 		/// Ordered top to bottom and then left to right.
 		/// </summary>
 		TOP_TO_BOTTOM_FIRST
-	}
-
-	/// <summary>
-	/// Options that are valid for <see cref="GBRObjectTileSettings.ColorSet"/>.
-	/// </summary>
-	public enum ColorSet : byte
-	{
-		GAMEBOY_POCKET = 0,
-		GAMEBOY = 1,
-		GAMEBOY_COLOR = 2,
-		SUPER_GAMEBOY = 3,
-		GAMEBOY_COLOR_FILTERED = 4
 	}
 }
