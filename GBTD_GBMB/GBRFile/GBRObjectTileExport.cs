b19094769436423a68c7d2932dce56a51c1997f8
Pokechu22 <pokechu022@gmail.com>
2015-02-23 11:50:29 -0800

    Created all of the feilds for GBRObjectTileExport

--- GBTD_GBMB/GBRFile/GBRObjectTileExport.cs
+++ GBTD_GBMB/GBRFile/GBRObjectTileExport.cs
@@ -12,6 +12,112 @@ namespace GB.Shared.GBRFile
 		public GBRObjectTileExport(UInt16 TypeID, UInt16 UniqueID, UInt32 Size, Stream stream) : base(TypeID, UniqueID, Size, stream) { }
 		public GBRObjectTileExport(GBRObjectHeader header, Stream stream) : base(header, stream) { }
 
+		/// <summary>
+		/// The coresponding object ID.
+		/// </summary>
+		/// <remarks>Since: Initial version</remarks>
+		public UInt16 CorrespondingID { get; set; }
+		/// <summary>
+		/// The name of the file to export to.
+		/// </summary>
+		/// <remarks>Since: Initial version</remarks>
+		public string FileName { get; set; }
+		/// <summary>
+		/// The type of the file.
+		/// </summary>
+		/// <remarks>Since: Initial version</remarks>
+		public ExportFileType FileType { get; set; }
+		/// <summary>
+		/// The name of the section (when using ASM).
+		/// </summary>
+		/// <remarks>Since: Initial version</remarks>
+		public string SectionName { get; set; }
+		/// <summary>
+		/// The name of the label / identifier to use.
+		/// </summary>
+		/// <remarks>Since: Initial version</remarks
+		public string LabelName { get; set; }
+		/// <summary>
+		/// The bank to store it in.
+		/// </summary>
+		/// <remarks>Since: Initial version</remarks
+		public byte Bank { get; set; }
+		/// <summary>
+		/// Store the tiles in an array?
+		/// </summary>
+		/// <remarks>Since: Initial version</remarks
+		public bool StoreTilesInArray { get; set; }
+		/// <summary>
+		/// The format to use for the tile data.
+		/// </summary>
+		/// <remarks>Since: Initial version</remarks
+		public ExportFormat Format { get; set; }
+		/// <summary>
+		/// The type of counter to use.
+		/// </summary>
+		/// <remarks>Since: Initial version</remarks
+		public ExportCounterType CounterType { get; set; }
+		/// <summary>
+		/// The first tile to export.
+		/// </summary>
+		/// <remarks>Since: Initial version</remarks
+		public UInt16 FromTile { get; set; }
+		/// <summary>
+		/// The last tile to export.
+		/// </summary>
+		/// <remarks>Since: Initial version</remarks
+		public UInt16 ToTile { get; set; }
+		/// <summary>
+		/// Whether or not GBCompression should be used.
+		/// </summary>
+		/// <remarks>Since: Initial version</remarks
+		public ExportCompressionMode UseCompression { get; set; }
+		/// <summary>
+		/// Whether or not palette colors should be included.
+		/// </summary>
+		/// <remarks>Since: GBTD 1.3</remarks
+		public bool IncludeColors { get; set; }
+		/// <summary>
+		/// The mode used for SGB palettes, if they are enabled.
+		/// </summary>
+		/// <remarks>Since: GBTD 1.4</remarks
+		public ExportPaletteMode SGBPalMode { get; set; }
+		/// <summary>
+		/// The mode used for GBC palettes, if they are enabled.
+		/// </summary>
+		/// <remarks>Since: GBTD 1.4</remarks
+		public ExportPaletteMode GBCPalMode { get; set; }
+		/// <summary>
+		/// Whether or not meta tiles should be generated.
+		/// </summary>
+		/// <remarks>Since: GBTD 1.5</remarks
+		public bool MakeMetaTiles { get; set; }
+		/// <summary>
+		/// The offset for meta tiles.
+		/// </summary>
+		/// <remarks>Since: GBTD 1.5</remarks
+		public UInt32 MetaTileOffset { get; set; }
+		/// <summary>
+		/// The format used for the MetaTile counter.
+		/// </summary>
+		/// <remarks>Since: GBTD 1.5</remarks
+		public ExportCounterType MetaCounterFormat { get; set; }
+		/// <summary>
+		/// Whether data should be split.
+		/// </summary>
+		/// <remarks>Since: GBTD 1.8</remarks
+		public bool Split { get; set; }
+		/// <summary>
+		/// The size to split blocks into.
+		/// </summary>
+		/// <remarks>Since: GBTD 1.8</remarks
+		public UInt32 BlockSize { get; set; }
+		/// <summary>
+		/// The currently selected tab.
+		/// </summary>
+		/// <remarks>Since: GBTD 1.8</remarks
+		public byte SelectedTab { get; set; }
+
 		protected override void SaveToStream(Stream s) {
 			throw new NotImplementedException();
 		}
