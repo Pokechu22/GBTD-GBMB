b81e03deca9b6e81080e23a604b3c97633b3bee7
Pokechu22 <pokechu022@gmail.com>
2015-03-03 11:55:34 -0800

    Created SaveToStream method.

--- GBTD_GBMB/GBMFile/GBMObjectMapTileData.cs
+++ GBTD_GBMB/GBMFile/GBMObjectMapTileData.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 
 namespace GB.Shared.GBMFile
 {
@@ -47,7 +48,38 @@ namespace GB.Shared.GBMFile
 				set { flippedVertically = value; }
 			}
 
-			//TODO: Saving, loading, constructors.
+			public void SaveToStream(Stream s) {
+				byte b0 = s.ReadByteEx();
+				byte b1 = s.ReadByteEx();
+				byte b2 = s.ReadByteEx();
+
+				long l = (b2 << 0) | (b1 << 8) | (b0 << 16);
+
+				this.tileNumber = (UInt16)BitRange(l, 0, 10);
+				this.GBCPalette = (byte)BitRange(l, 10, 5);
+				this.unused1 = BitRange(l, 15, 1) != 0;
+				this.sgbPalette = (byte)BitRange(l, 16, 3);
+				this.unused2 = (byte)BitRange(l, 19, 3);
+				this.flippedHorizontally = BitRange(l, 22, 1) != 0;
+				this.flippedVertically = BitRange(l, 23, 1) != 0;
+			}
+
+			/// <summary>
+			/// Gets the range of bits specified, shifted right at the start.
+			/// TODO better doc for this method.
+			/// </summary>
+			private long BitRange(long l, ushort start, ushort length) {
+				long mask = 0;
+				for (ushort i = start; i < start + length; i++) {
+					mask |= (1U << i);
+				}
+
+				return (l & mask) >> start;
+			}
+
+			public GBMObjectMapTileDataRecord(Stream s) {
+
+			}
 		}
 		//TODO: Everything.
 	}
