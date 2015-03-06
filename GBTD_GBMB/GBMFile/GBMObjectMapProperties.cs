9bce4c577fa3ffb685f8e0507dc07d9ded6dd6e4
Pokechu22 <pokechu022@gmail.com>
2015-03-06 09:57:49 -0800

    Moved records to seperate files.
    Also, I manually edited the .csproj file to make the appear as sub-items
    of the main object.  Science!

--- GBTD_GBMB/GBMFile/GBMObjectMapProperties.cs
+++ GBTD_GBMB/GBMFile/GBMObjectMapProperties.cs
@@ -17,46 +17,6 @@ namespace GB.Shared.GBMFile
 
 		public GBMObjectMapProperties(GBMObjectMap Master, GBMObjectHeader header, Stream stream) : base(Master, header, stream) { }
 
-		public class GBMObjectMapPropertiesRecord
-		{
-			/// <summary>
-			/// The type of the property.
-			/// This value does not appear to be used.
-			/// </summary>
-			public UInt32 Type { get; set; }
-			/// <summary>
-			/// The maximum value for the property.
-			/// The docs name this "Size", but that seems like a misnomer.
-			/// </summary>
-			public UInt32 MaxValue { get; set; }
-			/// <summary>
-			/// The name of the property.
-			/// </summary>
-			public String Name { get; set; }
-
-			public GBMObjectMapPropertiesRecord(Stream s) {
-				this.Type = s.ReadInteger();
-				this.MaxValue = s.ReadInteger();
-				this.Name = s.ReadString(32);
-			}
-
-			public void SaveToStream(Stream s) {
-				s.WriteInteger(Type);
-				s.WriteInteger(MaxValue);
-				s.WriteString(Name, 32);
-			}
-
-			public TreeNode ToTreeNode(string name) {
-				TreeNode node = new TreeNode(name);
-
-				node.Nodes.Add("Type", "Type: " + Type.ToString("X8"));
-				node.Nodes.Add("MaxValue", "MaxValue: " + MaxValue);
-				node.Nodes.Add("Name", "Name: " + Name);
-
-				return node;
-			}
-		}
-
 		public GBMObjectMapPropertiesRecord[] Properties { get; set; }
 
 		protected override void LoadFromStream(Stream s) {
---
+++ GBTD_GBMB/GBMFile/GBMObjectMapPropertiesRecord.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+
+namespace GB.Shared.GBMFile
+{
+	/// <summary>
+	/// A single record contained within a <see cref="GBMObjectMapProperties"/>
+	/// </summary>
+	public class GBMObjectMapPropertiesRecord
+	{
+		/// <summary>
+		/// The type of the property.
+		/// This value does not appear to be used.
+		/// </summary>
+		public UInt32 Type { get; set; }
+		/// <summary>
+		/// The maximum value for the property.
+		/// The docs name this "Size", but that seems like a misnomer.
+		/// </summary>
+		public UInt32 MaxValue { get; set; }
+		/// <summary>
+		/// The name of the property.
+		/// </summary>
+		public String Name { get; set; }
+
+		public GBMObjectMapPropertiesRecord(Stream s) {
+			this.Type = s.ReadInteger();
+			this.MaxValue = s.ReadInteger();
+			this.Name = s.ReadString(32);
+		}
+
+		public void SaveToStream(Stream s) {
+			s.WriteInteger(Type);
+			s.WriteInteger(MaxValue);
+			s.WriteString(Name, 32);
+		}
+
+		public TreeNode ToTreeNode(string name) {
+			TreeNode node = new TreeNode(name);
+
+			node.Nodes.Add("Type", "Type: " + Type.ToString("X8"));
+			node.Nodes.Add("MaxValue", "MaxValue: " + MaxValue);
+			node.Nodes.Add("Name", "Name: " + Name);
+
+			return node;
+		}
+	}
+}
