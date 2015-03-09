0b04534d1652d741ac2d8377376a20c0294b3f3a
Pokechu22 <pokechu022@gmail.com>
2015-03-09 09:59:31 -0700

    Started on the GBMB project.

---
+++ GBTD_GBMB/GBMB/MapControl.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.ComponentModel;
+using GB.Shared.Palettes;
+using GB.Shared.GBMFile;
+
+namespace GB.GBMB
+{
+	public class MapControl : Control
+	{
+		[Category("Map data"), Description("The width of the Map.")]
+		public UInt32 MapWidth { get; set; }
+		[Category("Map data"), Description("The height of the Map.")]
+		public UInt32 MapHeight { get; set; }
+
+		/// <summary>
+		/// The zoom used.  TODO: Enum?
+		/// </summary>
+		[Category("Map display"), Description("The zoom to use for the map.")]
+		public UInt16 Zoom { get; set; }
+
+		[Category("Map data"), Description("The color set to use for the map.")]
+		public ColorSet ColorSet { get; set; }
+		[Category("Map data"), Description("The palette set to use for the map.")]
+		public PaletteSet PaletteSet { get; set; }
+
+		[Category("Map data"), Description("The map to use.")]
+		public GBMObjectMapTileData Map { get; set; }
+	}
+}
---
+++ GBTD_GBMB/GBMB/MapEdit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GB.GBMB
+{
+	public partial class MapEdit : Form
+	{
+		public MapEdit() {
+			InitializeComponent();
+		}
+	}
+}
