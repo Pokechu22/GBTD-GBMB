66ea2645d2ea87c8567b515dfde563e4133dbd5b
Pokechu22 <pokechu022@gmail.com>
2015-03-16 08:46:37 -0700

    Created the rest of the events.

--- GBTD_GBMB/GBAutoUpdate/AUListener.cs
+++ GBTD_GBMB/GBAutoUpdate/AUListener.cs
@@ -105,14 +105,45 @@ namespace GB.Shared.AutoUpdate
 
 		void onListenerMessage(object sender, MessageEventArgs args) {
 			if (args.Message.Msg == AutoUpdateMessageID) {
+				UInt16 WParam = (UInt16)args.Message.WParam;
+				if ((WParam & 0x8000U) != 0) {
+					//A special message type has occured.
+				} else {
+
+				}
+
+				//Finally, send the full change message.
 				if (OnAutoUpdateMessage != null) {
 					OnAutoUpdateMessage(this, args);
 				}
 			}
 		}
 
+		[Description("Raw event for accessing the raw message used")]
 		public event MessageEventHandler OnAutoUpdateMessage;
+		[Description(@"Called when a tile changes.
+Changes can be different pixels, or changes in the selected palette entries for this type.")]
 		public event TileChangedEventHandler OnTileChanged;
+		[Description(@"Called when a total refresh is necessary.
+The TILEMSGTOTAL (0x8000) message (or an unrecognised one) was sent.
+A full and total refresh of all parts of the tileset should take place. This can be seen as a combination of all other messages.")]
+		public event EventHandler OnTotalRefreshNeeded;
+		[Description(@"The graphics and palettes for all tiles should be refreshed, but nothing else.
+The TILEMSGLIST (0x8001) message was sent.
+All tiles should be refreshed, but only the actual pixels and palette entries. Note that this is also called when the actual tile count has changed.")]
+		public event EventHandler OnTileRefreshNeeded;
+		[Description(@"The size of the tiles (height and width) has been changed.
+The TILEMSGDIM (0x8002) message was sent.
+The dimensions of the tiles (“Tile size..”) have changed.")]
+		public event EventHandler OnTileSizeChanged;
+		[Description(@"The DMG palette has changed.
+The TILEMSGPAL (0x8003) message was sent.
+The Gameboy palette has changed (ie: the values used by IO-ports $47-$49, not the color palettes).")]
+		public event EventHandler OnGBPaletteChanged;
+		[Description(@"One of the color palettes has changed.
+The TILEMSGCOLSETS (0x8004) message was sent.
+Either the GBC or SGB palette colors have changed.")]
+		public event EventHandler OnColorPaletteChanged;
 	}
 
 	/// <summary>
