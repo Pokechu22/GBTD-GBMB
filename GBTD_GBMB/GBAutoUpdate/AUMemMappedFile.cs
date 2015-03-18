0fc43146e3d3027ae441d94d333f34db366083ca
Pokechu22 <pokechu022@gmail.com>
2015-03-18 08:02:13 -0700

    Copied in yesterday's code for MemoryMappedFiles.
    I didn't have my USB so this is a bit messy and doesn't use the real
    types.

---
+++ GBTD_GBMB/GBAutoUpdate/AUMemMappedFile.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO.MemoryMappedFiles;
+using System.IO;
+using System.ComponentModel;
+using System.Drawing;
+
+namespace GB.Shared.AutoUpdate
+{
+	/// <summary>
+	/// Provides access to the data contained in a MemoryMappedFile.
+	/// </summary>
+	public class AUMemMappedFile : IDisposable
+	{
+		#region Inner classes (for named indexers)
+		/// <summary>
+		/// List of tiles.  TODO: Use tile instead of byte array.
+		/// </summary>
+		public class MMFTileList
+		{
+			private readonly AUMemMappedFile file;
+			internal MMFTileList(AUMemMappedFile file) {
+				this.file = file;
+			}
+
+			public byte[,] this[UInt16 tile] {
+				get {
+					var stream = file.stream;
+					stream.Position = TILES_INDEX + (tile * 8 * 8);
+
+					byte[] data = new byte[8 * 8];
+					var read = stream.Read(data, 0, 8 * 8);
+					if (read != 8 * 8) {
+						throw new EndOfStreamException();
+					}
+
+					byte[,] result = new byte[8, 8];
+
+					for (int y = 0; y < 8; y++) {
+						for (int x = 0; x < 8; x++) {
+							result[x, y] = data[x + (y * 8) + (tile * 8 * 8)];
+						}
+					}
+
+					return result;
+				}
+				set {
+					var stream = file.stream;
+					stream.Position = TILES_INDEX + (tile * 8 * 8);
+
+					byte[] data = new byte[8 * 8];
+
+					if (value.GetLength(0) != 8) { throw new ArgumentOutOfRangeException("value", "0th length is invalid (must be 8)"); }
+					if (value.GetLength(1) != 8) { throw new ArgumentOutOfRangeException("value", "1th length is invalid (must be 8)"); }
+
+					for (int y = 0; y < 8; y++) {
+						for (int x = 0; x < 8; x++) {
+							data[x + (y * 8)] = value[x, y];
+						}
+					}
+
+					stream.Write(data, 0, 8 * 8);
+				}
+			}
+		}
+
+		public class MMFPalMapList
+		{
+			public struct PalMapEntry {
+				public PalMapEntry(byte GBC, byte SGB) {
+					this.GBC = GBC;
+					this.SGB = SGB;
+				}
+
+				public byte GBC;
+				public byte SGB;
+
+				public override string ToString() {
+					return "GBC: " + GBC + "; SGB: " + SGB;
+				}
+			}
+
+			private readonly AUMemMappedFile file;
+			internal MMFPalMapList(AUMemMappedFile file) {
+				this.file = file;
+			}
+
+			public PalMapEntry this[UInt16 tile] {
+				get {
+					var stream = file.stream;
+					stream.Position = PALMAPS_INDEX + (tile * 2);
+
+					byte gbc = stream.ReadByteEx();
+					byte sgb = stream.ReadByteEx();
+
+					return new PalMapEntry(gbc, sgb);
+				}
+				set {
+					var stream = file.stream;
+					stream.Position = PALMAPS_INDEX + (tile * 2);
+
+					stream.WriteByte(value.GBC);
+					stream.WriteByte(value.SGB);
+				}
+			}
+		}
+
+		/// <summary>
+		/// Color set.  TODO: Use the more official type - palette, ect.
+		/// </summary>
+		public class MMFGBColorSet
+		{
+			/// <summary>
+			/// The size of a color.
+			/// </summary>
+			private const int SIZE_OF_COLOR = 4;
+			private const int NUM_OF_ENTRIES = 4;
+
+			private readonly AUMemMappedFile file;
+			/// <summary>
+			/// THe index used, because this class uses two different indecies.
+			/// TODO: this seems like an ugly sollution.
+			/// </summary>
+			private readonly int USED_INDEX;
+
+			internal MMFGBColorSet(AUMemMappedFile file, int usedIndex) {
+				this.file = file;
+			}
+
+			public Color this[int row, int pal] {
+				get {
+					var stream = file.stream;
+					stream.Position = USED_INDEX + (row * NUM_OF_ENTRIES * SIZE_OF_COLOR) + (pal * SIZE_OF_COLOR);
+
+					return ReadColor(stream);
+				}
+				set {
+					var stream = file.stream;
+					stream.Position = USED_INDEX + (row * NUM_OF_ENTRIES * SIZE_OF_COLOR) + (pal * SIZE_OF_COLOR);
+
+					WriteColor(stream, value);
+				}
+			}
+
+			public Color[] this[int row] {
+				get {
+					var stream = file.stream;
+					stream.Position = USED_INDEX + (row * NUM_OF_ENTRIES * SIZE_OF_COLOR);
+
+					Color[] colors = new Color[NUM_OF_ENTRIES];
+
+					for (int i = 0; i < colors.Length; i++) {
+						colors[i] = ReadColor(stream);
+					}
+
+					return colors;
+				}
+				set {
+					var stream = file.stream;
+					stream.Position = USED_INDEX + (row * NUM_OF_ENTRIES * SIZE_OF_COLOR);
+
+					if (value.Length != NUM_OF_ENTRIES) {
+						throw new ArgumentException("Number of provided entries is incorrect - should be exactly " + NUM_OF_ENTRIES + ".", "value");
+					}
+
+					for (int i = 0; i < value.Length; i++) {
+						WriteColor(stream, value[i]);
+					}
+				}
+			}
+
+			private Color ReadColor(Stream s) {
+				byte[] bytes = new byte[4];
+				int read = s.Read(bytes, 0, 4);
+				
+				if (read != 4) {
+					throw new EndOfStreamException();
+				}
+
+				//Intentionally ignoring the byte in index 0, as it is alpha and unused by GBTD.
+
+				return Color.FromArgb(bytes[1], bytes[2], bytes[3]);
+			}
+
+			private void WriteColor(Stream s, Color color) {
+				s.WriteByteEx(0xFF); //Ignored alpha value.
+				s.WriteByteEx((byte)color.R);
+				s.WriteByteEx((byte)color.G);
+				s.WriteByteEx((byte)color.B);
+			}
+		}
+		#endregion
+
+		//All of the indecies of the different features of the file.
+		private const int ID_INDEX = 0, ID_SIZE = 4;
+		private const int TILES_INDEX = 4, TILES_SIZE = 8 * 8 * 768;
+		private const int PALMAPS_INDEX = 49156, PALMAPS_SIZE = 768 * 2;
+		private const int TILECOUNT_INDEX = 50692, TILECOUNT_SIZE = 4;
+		private const int TILEWIDTH_INDEX = 50696, TILEWIDTH_SIZE = 4;
+		private const int TILEHEIGHT_INDEX = 50700, TILEHEIGHT_SIZE = 4;
+		private const int GBPAL_INDEX = 50704, GBPAL_SIZE = 50704;
+		private const int GBCCOLSET_INDEX = 50708, GBCCOLSET_SIZE = 8 * 4 * 4;
+		private const int SGBCOLSET_INDEX = 50836, SGBCOLSET_SIZE = 8 * 4 * 4;
+		//The size of the memory block, in total.
+		private const int MEM_BLOCK_SIZE = 50964;
+
+		private MemoryMappedFile file;
+		private MemoryMappedViewStream stream;
+
+		public AUMemMappedFile(String fileName) {
+			//The hidden step.
+			fileName = fileName.ToUpperInvariant().Replace('\\', '@');
+
+			try {
+				this.file = MemoryMappedFile.OpenExisting(fileName, MemoryMappedFileRights.ReadWrite);
+			} catch (FileNotFoundException) {
+				//TODO initialize the defaults.
+				//this.file = MemoryMappedFile.CreateNew(fileName, MEM_BLOCK_SIZE, MemoryMappedFileAccess.ReadWrite);
+				//TODO send the message for everything to update.
+
+				throw; //TODO We don't want to set the default right now.
+			}
+
+			this.stream = file.CreateViewStream();
+
+			Tiles = new MMFTileList(this);
+			PalMaps = new MMFPalMapList(this);
+		}
+
+		~AUMemMappedFile() {
+			this.Dispose();
+		}
+
+		public void Dispose() {
+			try {
+				file.Dispose();
+				stream.Dispose();
+			} finally {
+
+			}
+		}
+
+		/// <summary>
+		/// The ID of the file.
+		/// </summary>
+		public string ID {
+			get {
+				stream.Position = ID_INDEX;
+				return stream.ReadString(ID_SIZE);
+			}
+			set {
+				stream.Position = ID_INDEX;
+				stream.WriteString(value, ID_SIZE);
+			}
+		}
+
+		public MMFTileList Tiles { get; private set; }
+		public MMFPalMapList PalMaps { get; private set; }
+
+		public UInt32 TileCount {
+			get {
+				stream.Position = TILECOUNT_INDEX;
+				return stream.ReadInteger();
+			}
+			set {
+				stream.Position = TILECOUNT_INDEX;
+				stream.WriteInteger(value);
+			}
+		}
+
+		public UInt32 TileWidth {
+			get {
+				stream.Position = TILEWIDTH_INDEX;
+				return stream.ReadInteger();
+			}
+			set {
+				stream.Position = TILEWIDTH_INDEX;
+				stream.WriteInteger(value);
+			}
+		}
+
+		public UInt32 TileHeight {
+			get {
+				stream.Position = TILEHEIGHT_INDEX;
+				return stream.ReadInteger();
+			}
+			set {
+				stream.Position = TILEHEIGHT_INDEX;
+				stream.WriteInteger(value);
+			}
+		}
+
+		/// <summary>
+		/// The DMG palettes.  
+		/// TODO make this a struct, and use GBColor.
+		/// </summary>
+		public byte[] GBPalettes {
+			get {
+				stream.Position = GBPAL_INDEX;
+
+				byte[] bytes = new byte[4];
+				int read = stream.Read(bytes, 0, 4);
+
+				if (read != 4) {
+					throw new EndOfStreamException();
+				}
+
+				return bytes;
+			}
+			set {
+				stream.Position = GBPAL_INDEX;
+
+				if (value.Length != 4) {
+					throw new ArgumentException("Value must be of length 4", "value");
+				}
+
+				stream.Write(value, 0, 4);
+			}
+		}
+		
+		//TODO: GBCPalettes and SGBPalettes.
+	}
+}
---
+++ GBTD_GBMB/GBAutoUpdate/StreamExtensions.cs
@@ -0,0 +1,406 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace GB.Shared.AutoUpdate
+{
+	/// <summary>
+	/// Provides a general set of functions that handle writing things to streams in compliance with the GBM format definitions.
+	/// 
+	/// <para>For reference: </para>
+	/// 
+	/// <para>Word -> 16-bit hi-endian.</para>
+	/// <para>Unsigned long -> 32-bit hi-endian.</para>
+	/// <para>Integer -> 32-bit hi-endian, unsigned.</para>
+	/// <para>String (xx) -> C-style string; ie. ends with hex 00, with a maximum length of xx (including end-marker).
+	/// This is padded with 0s if the end has not been reached..</para>
+	/// </summary>
+	internal static class StreamExtensions
+	{
+		#region Byte-handling methods
+		/// <summary>
+		/// Reads a single byte from the stream, throwing an exception if at the end.
+		/// </summary>
+		/// <param name="stream">The stream to read from</param>
+		/// <exception cref="EndOfStreamException">When at the end of the stream</exception>
+		/// <exception cref="NotSupportedException">When the stream does not support reading.</exception>
+		internal static byte ReadByteEx(this Stream stream) {
+			if (!stream.CanRead) { throw new NotSupportedException("Stream cannot be read!"); }
+
+			byte[] bytes = new byte[1];
+			int read = stream.Read(bytes, 0, 1);
+
+			if (read != 1) { throw new EndOfStreamException(); }
+
+			return bytes[0];
+		}
+
+		/// <summary>
+		/// Reads a single byte from the stream, returning default if at the end.
+		/// </summary>
+		/// <param name="stream">The stream to read from</param>
+		/// <param name="def">The default value to return.</param>
+		/// <exception cref="NotSupportedException">When the stream does not support reading.</exception>
+		internal static byte ReadByteEx(this Stream stream, byte def) {
+			if (!stream.CanRead) { throw new NotSupportedException("Stream cannot be read!"); }
+
+			byte[] bytes = new byte[1];
+			int read = stream.Read(bytes, 0, 1);
+
+			if (read != 1) { return def; }
+
+			return bytes[0];
+		}
+
+		/// <summary>
+		/// Writes a single byte to the stream.
+		/// <para>Yes, this is somewhat redundant, but it seems like a good idea to include it for consistency.</para>
+		/// </summary>
+		/// <param name="stream">The stream to write to.</param>
+		/// <param name="value">The byte to write.</param>
+		/// <exception cref="NotSupportedException">When the stream cannot be written to.</exception>
+		internal static void WriteByteEx(this Stream stream, byte value) {
+			if (!stream.CanWrite) { throw new NotSupportedException("Stream cannot be written to!"); }
+
+			stream.WriteByte(value);
+		}
+		#endregion
+
+		#region Word-handling methods
+		/// <summary>
+		/// Reads a 16-bit (2 byte) unsigned number, hi-endian, from the specified stream, throwing an exception if at the end.
+		/// <param name="stream">The stream to read from.</param>
+		/// </summary>
+		/// <exception cref="NotSupportedException">When the stream cannot be read from.</exception>
+		/// <exception cref="EndOfStreamException">When the end of the stream has been reached.</exception>
+		internal static UInt16 ReadWord(this Stream stream) {
+			if (!stream.CanRead) {
+				throw new NotSupportedException("Stream cannot be read from.");
+			}
+
+			byte[] bytes = new byte[2];
+			int read = stream.Read(bytes, 0, 2);
+
+			if (read != 2) {
+				throw new EndOfStreamException();
+			}
+
+			return (UInt16)((bytes[1] << 8) | (bytes[0] << 0));
+		}
+
+		/// <summary>
+		/// Reads a 16-bit (2 byte) unsigned number, hi-endian, from the specified stream, returning the default value if at the end.
+		/// </summary>
+		/// <param name="stream">The stream to read from.</param>
+		/// <param name="def">The default value to return if at the end of the stream.</param>
+		/// <exception cref="NotSupportedException">When the stream cannot be read from.</exception>
+		internal static UInt16 ReadWord(this Stream stream, UInt16 def) {
+			if (!stream.CanRead) {
+				throw new NotSupportedException("Stream cannot be read from.");
+			}
+
+			byte[] bytes = new byte[2];
+			int read = stream.Read(bytes, 0, 2);
+
+			if (read != 2) {
+				return def;
+			}
+
+			return (UInt16)((bytes[1] << 8) | (bytes[0] << 0));
+		}
+
+		/// <summary>
+		/// Writes a 16-bit (2 byte) unsigned number, hi-endian, to the specified stream.
+		/// <param name="stream">The stream to write to.</param>
+		/// <param name="n">The number to write.</param>
+		/// </summary>
+		/// <exception cref="NotSupportedException">When the stream cannot be written to.</exception>
+		internal static void WriteWord(this Stream stream, UInt16 n) {
+			if (!stream.CanWrite) {
+				throw new NotSupportedException("Stream cannot be written to.");
+			}
+
+			stream.WriteByte((byte)((n >> 0) & 0xFF));
+			stream.WriteByte((byte)((n >> 8) & 0xFF));
+		}
+		#endregion
+
+		#region Unsigned Long-handling methods
+		/// <summary>
+		/// Reads a 32-bit (4 byte) unsigned number, hi-endian, from the specified stream, throwing an exception if at the end of the stream.
+		/// <para>Despite the fact that this method name says that it uses ULongs, the actual type is UInt32.  Just the way the spec works.</para>
+		/// </summary>
+		/// <param name="stream">The stream to write to.</param>
+		/// <exception cref="NotSupportedException">When the stream cannot be read.</exception>
+		/// <exception cref="EndOfStreamException">When the end of the stream has been reached.</exception>
+		internal static UInt32 ReadUnsignedLong(this Stream stream) {
+			if (!stream.CanRead) {
+				throw new NotSupportedException("Stream cannot be read from.");
+			}
+
+			byte[] bytes = new byte[4];
+			int read = stream.Read(bytes, 0, 4);
+
+			if (read != 4) {
+				throw new EndOfStreamException();
+			}
+
+			return (UInt32)((bytes[3] << 24) | (bytes[2] << 16) | (bytes[1] << 8) | (bytes[0] << 0));
+		}
+
+		/// <summary>
+		/// Reads a 32-bit (4 byte) unsigned number, hi-endian, from the specified stream, returning the default if at end of stream.
+		/// <para>Despite the fact that this method name says that it uses ULongs, the actual type is UInt32.  Just the way the spec works.</para>
+		/// </summary>
+		/// <param name="stream">The stream to write to.</param>
+		/// <param name="def">The default value to return if at the end of the stream.</param>
+		/// <exception cref="NotSupportedException">When the stream cannot be read.</exception>
+		internal static UInt32 ReadUnsignedLong(this Stream stream, UInt32 def) {
+			if (!stream.CanRead) {
+				throw new NotSupportedException("Stream cannot be read from.");
+			}
+
+			byte[] bytes = new byte[4];
+			int read = stream.Read(bytes, 0, 4);
+
+			if (read != 4) {
+				return def;
+			}
+
+			return (UInt32)((bytes[3] << 24) | (bytes[2] << 16) | (bytes[1] << 8) | (bytes[0] << 0));
+		}
+
+		/// <summary>
+		/// Writes a 32-bit (4 byte) unsigned number, hi-endian, to the specified stream.
+		/// <para>Despite the fact that this method name says that it uses ULongs, the actual type is UInt32.  Just the way the spec works.</para>
+		/// <param name="stream">The stream to write to.</param>
+		/// <param name="n">The number to write.</param>
+		/// </summary>
+		/// <exception cref="NotSupportedException">When the stream cannot be written to.</exception>
+		internal static void WriteUnsignedLong(this Stream stream, UInt32 n) {
+			if (!stream.CanWrite) {
+				throw new NotSupportedException("Stream cannot be written to.");
+			}
+
+			stream.WriteByte((byte)((n >> 0) & 0xFF));
+			stream.WriteByte((byte)((n >> 8) & 0xFF));
+			stream.WriteByte((byte)((n >> 16) & 0xFF));
+			stream.WriteByte((byte)((n >> 24) & 0xFF));
+		}
+		#endregion
+
+		#region Integer-handling methods
+		/// <summary>
+		/// Reads a 32-bit (4 byte) unsigned number, hi-endian, from the specified stream, throwing an exception if at the end of the stream.
+		/// </summary>
+		/// <param name="stream">The stream to write to.</param>
+		/// <exception cref="NotSupportedException">When the stream cannot be read.</exception>
+		/// <exception cref="EndOfStreamException">When the end of the stream has been reached.</exception>
+		internal static UInt32 ReadInteger(this Stream stream) {
+			if (!stream.CanRead) {
+				throw new NotSupportedException("Stream cannot be read from.");
+			}
+
+			byte[] bytes = new byte[4];
+			int read = stream.Read(bytes, 0, 4);
+
+			if (read != 4) {
+				throw new EndOfStreamException();
+			}
+
+			return (UInt32)((bytes[3] << 24) | (bytes[2] << 16) | (bytes[1] << 8) | (bytes[0] << 0));
+		}
+
+		/// <summary>
+		/// Reads a 32-bit (4 byte) unsigned number, hi-endian, from the specified stream, returning the default if at end of stream.
+		/// </summary>
+		/// <param name="stream">The stream to write to.</param>
+		/// <param name="def">The default value to return if at the end of the stream.</param>
+		/// <exception cref="NotSupportedException">When the stream cannot be read.</exception>
+		internal static UInt32 ReadInteger(this Stream stream, UInt32 def) {
+			if (!stream.CanRead) {
+				throw new NotSupportedException("Stream cannot be read from.");
+			}
+
+			byte[] bytes = new byte[4];
+			int read = stream.Read(bytes, 0, 4);
+
+			if (read != 4) {
+				return def;
+			}
+
+			return (UInt32)((bytes[3] << 24) | (bytes[2] << 16) | (bytes[1] << 8) | (bytes[0] << 0));
+		}
+
+		/// <summary>
+		/// Writes a 32-bit (4 byte) unsigned number, hi-endian, to the specified stream.
+		/// <param name="stream">The stream to write to.</param>
+		/// <param name="n">The number to write.</param>
+		/// </summary>
+		/// <exception cref="NotSupportedException">When the stream cannot be written to.</exception>
+		internal static void WriteInteger(this Stream stream, UInt32 n) {
+			if (!stream.CanWrite) {
+				throw new NotSupportedException("Stream cannot be written to.");
+			}
+
+			stream.WriteByte((byte)((n >> 0) & 0xFF));
+			stream.WriteByte((byte)((n >> 8) & 0xFF));
+			stream.WriteByte((byte)((n >> 16) & 0xFF));
+			stream.WriteByte((byte)((n >> 24) & 0xFF));
+		}
+		#endregion
+
+		#region Boolean-handling methods
+		/// <summary>
+		/// Reads a boolean from the stream, throwing an exception if at the end.
+		/// </summary>
+		/// <param name="stream">The stream to read from</param>
+		/// <exception cref="EndOfStreamException">When at the end of the stream</exception>
+		/// <exception cref="NotSupportedException">When the stream does not support reading.</exception>
+		internal static bool ReadBoolean(this Stream stream) {
+			if (!stream.CanRead) { throw new NotSupportedException("Stream cannot be read!"); }
+
+			return stream.ReadByteEx() != 0;
+		}
+
+		/// <summary>
+		/// Reads a boolean from the stream, returning default if at the end.
+		/// </summary>
+		/// <param name="stream">The stream to read from</param>
+		/// <param name="def">The default value to return.</param>
+		/// <exception cref="NotSupportedException">When the stream does not support reading.</exception>
+		internal static bool ReadBoolean(this Stream stream, bool def) {
+			if (!stream.CanRead) { throw new NotSupportedException("Stream cannot be read!"); }
+
+			return stream.ReadByteEx((byte)(def ? 1 : 0)) != 0;
+		}
+
+		/// <summary>
+		/// Writes a boolean to the stream.
+		/// </summary>
+		/// <param name="stream">The stream to write to.</param>
+		/// <param name="value">The bool to write.</param>
+		/// <exception cref="NotSupportedException">When the stream cannot be written to.</exception>
+		internal static void WriteBoolean(this Stream stream, bool value) {
+			if (!stream.CanWrite) { throw new NotSupportedException("Stream cannot be written to!"); }
+
+			stream.WriteByteEx((byte)(value ? 1 : 0));
+		}
+		#endregion
+
+		#region String-handling methods
+		/// <summary>
+		/// Reads a string to the specified stream, in the expected format, throwing an exception if the end of the stream is reached.
+		/// <para>Per the doc: 
+		/// String (xx)	C-style string; ie. ends with hex 00, with a maximum length of xx (including end-marker).</para>
+		/// <para>This method will read until a null terminator or the end of the length, IE it will not return any <c>\0</c>'s in the string.</para>
+		/// <para>This method also uses ANSI for encoding.</para>
+		/// </summary>
+		/// <param name="stream">The stream to write to.</param>
+		/// <param name="length">The maximum expected length.</param>
+		/// <exception cref="NotSupportedException">When the stream cannot be read from.</exception>
+		/// <exception cref="EndOfStreamException">When the end of the stream has been reached.</exception>
+		/// <exception cref="ArgumentOutOfRangeException">When length is negative.</exception>
+		internal static String ReadString(this Stream stream, int length) {
+			//A length of 0 is legal (though pointless).
+			if (length < 0) {
+				throw new ArgumentOutOfRangeException("length", length, "Length must not be negative!");
+			}
+			if (!stream.CanRead) {
+				throw new NotSupportedException("Stream cannot be read from.");
+			}
+
+			byte[] bytes = new byte[length];
+			int read = stream.Read(bytes, 0, length);
+
+			if (read != length) {
+				throw new EndOfStreamException();
+			}
+
+			//Null termination.
+			String str = Encoding.ASCII.GetString(bytes);
+			if (str.Contains('\0')) {
+				return str.Remove(str.IndexOf('\0'));
+			} else {
+				return str;
+			}
+		}
+
+		/// <summary>
+		/// Reads a string to the specified stream, in the expected format, returning a default value if the stream has ended.
+		/// <para>Per the doc: 
+		/// String (xx)	C-style string; ie. ends with hex 00, with a maximum length of xx (including end-marker).</para>
+		/// <para>This method will read until a null terminator or the end of the length, IE it will not return any <c>\0</c>'s in the string.</para>
+		/// <para>This method also uses ANSI for encoding.</para>
+		/// </summary>
+		/// <param name="stream">The stream to write to.</param>
+		/// <param name="length">The maximum expected length.</param>
+		/// <param name="def">The default value to return.  Can be <c>null</c>.</param>
+		/// <exception cref="NotSupportedException">When the stream cannot be read from.</exception>
+		/// <exception cref="ArgumentOutOfRangeException">When length is negative.</exception>
+		internal static String ReadString(this Stream stream, int length, string def) {
+			//A length of 0 is legal (though pointless).
+			if (length < 0) {
+				throw new ArgumentOutOfRangeException("length", length, "Length must not be negative!");
+			}
+			if (!stream.CanRead) {
+				throw new NotSupportedException("Stream cannot be read from.");
+			}
+
+			byte[] bytes = new byte[length];
+			int read = stream.Read(bytes, 0, length);
+
+			if (read != length) {
+				return def;
+			}
+
+			//Null termination.
+			String str = Encoding.ASCII.GetString(bytes);
+			if (str.Contains('\0')) {
+				return str.Remove(str.IndexOf('\0'));
+			} else {
+				return str;
+			}
+		}
+		/// <summary>
+		/// Writes a string to the specified stream, in the expected format.
+		/// Reads a string to the specified stream, in the expected format, returning a default value if the stream has ended.
+		/// <para>Per the doc: 
+		/// String (xx)	C-style string; ie. ends with hex 00, with a maximum length of xx (including end-marker).</para>
+		/// <para>This method truncates the string if necessary.</para>
+		/// <para>This method also uses ANSI for encoding.</para>
+		/// </summary>
+		/// <param name="stream">The stream to write to.</param>
+		/// <param name="s">The string to write.</param>
+		/// <param name="length">The maximum allowed length.  Will be padded up to if needed.</param>
+		/// <exception cref="NotSupportedException">When the stream cannot be written to.</exception>
+		/// <exception cref="ArgumentOutOfRangeException">When length is negative.</exception>
+		/// <exception cref="ArgumentNullException">When s is null.</exception>
+		internal static void WriteString(this Stream stream, String s, uint length) {
+			if (s == null) {
+				throw new ArgumentNullException("s");
+			}
+			if (length < 0) {
+				throw new ArgumentOutOfRangeException("length", length, "Length must not be negative!");
+			}
+			if (!stream.CanWrite) {
+				throw new NotSupportedException("Stream cannot be written to.");
+			}
+			byte[] bytes = Encoding.ASCII.GetBytes(s);
+
+			//Go until the end -1, so that if a terminator is needed it shall be added.
+			for (int i = 0; i < length - 1; i++) {
+				if (i < bytes.Length) {
+					stream.WriteByte(bytes[i]);
+				} else {
+					stream.WriteByte(0x00);
+				}
+			}
+			//Force a terminator.
+			stream.WriteByte(0x00);
+		}
+		#endregion
+	}
+}
