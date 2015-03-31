b6af91fae1fab25cd8facebbc95cf89d0f6c9824
Pokechu22 <pokechu022@gmail.com>
2015-03-31 14:29:00 -0700

    GBMMapPropertyData now reacts to resizing.

--- GBTD_GBMB/GBMFile/GBMObjectMapPropertyData.cs
+++ GBTD_GBMB/GBMFile/GBMObjectMapPropertyData.cs
@@ -10,9 +10,16 @@ namespace GB.Shared.GBMFile
 	public class GBMObjectMapPropertyData : MasteredGBMObject<GBMObjectMap>
 	{
 		public GBMObjectMapPropertyData(GBMObjectMap Master, UInt16 TypeID, UInt16 UniqueID, UInt16? MasterID, UInt32 Size, Stream stream)
-				: base(Master, TypeID, UniqueID, MasterID, Size, stream) { }
+				: base(Master, TypeID, UniqueID, MasterID, Size, stream) {
 
-		public GBMObjectMapPropertyData(GBMObjectMap Master, GBMObjectHeader header, Stream stream) : base(Master, header, stream) { }
+			Master.SizeChanged += new EventHandler(Master_SizeChanged);
+			Master.PropCountChanged += new EventHandler(Master_PropCountChanged);
+		}
+
+		public GBMObjectMapPropertyData(GBMObjectMap Master, GBMObjectHeader header, Stream stream) : base(Master, header, stream) {
+			Master.SizeChanged += new EventHandler(Master_SizeChanged);
+			Master.PropCountChanged += new EventHandler(Master_PropCountChanged);
+		}
 
 		/// <summary>
 		/// The data for each property.
@@ -67,6 +74,53 @@ namespace GB.Shared.GBMFile
 			}
 
 			return root;
+		}
+
+		void Master_PropCountChanged(object sender, EventArgs e) {
+			uint width = (uint)Data.GetLength(0);
+			uint height = (uint)Data.GetLength(1);
+			uint oldPropCount = (uint)Data.GetLength(2);
+			uint newPropCount = Master.PropCount;
+
+			UInt16[, ,] newData = new UInt16[width, height, newPropCount];
+
+			for (int i = 0; i < newPropCount; i++) {
+				for (uint y = 0; y < height; y++) {
+					for (uint x = 0; x < width; x++) {
+						if (i >= oldPropCount) { //If the value would be out of bounds in the origional array.
+							newData[x, y, i] = 0;
+						} else {
+							newData[x, y, i] = Data[x, y, i];
+						}
+					}
+				}
+			}
+			
+			Data = newData;
+		}
+
+		void Master_SizeChanged(object sender, EventArgs e) {
+			uint propCount = (uint)Data.GetLength(2);
+			uint oldWidth = (uint)Data.GetLength(0);
+			uint oldHeight = (uint)Data.GetLength(1);
+			uint newWidth = Master.Width;
+			uint newHeight = Master.Height;
+
+			UInt16[, ,] newData = new UInt16[newWidth, newHeight, propCount];
+
+			for (int i = 0; i < propCount; i++) {
+				for (uint y = 0; y < newHeight; y++) {
+					for (uint x = 0; x < newWidth; x++) {
+						if (x >= oldWidth || y >= oldHeight) { //If the value would be out of bounds in the origional array.
+							newData[x, y, i] = 0;
+						} else {
+							newData[x, y, i] = Data[x, y, i];
+						}
+					}
+				}
+			}
+
+			Data = newData;
 		}
 	}
 }
