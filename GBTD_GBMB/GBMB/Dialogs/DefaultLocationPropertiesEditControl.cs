02a6f956e9dc9d8902de979ce56bffa3ddda3335
Pokechu22 <pokechu022@gmail.com>
2015-04-13 08:57:21 -0700

    Set up the text boxes in DefaultLocationPropertiesControl.

--- GBTD_GBMB/GBMB/Dialogs/DefaultLocationPropertiesEditControl.cs
+++ GBTD_GBMB/GBMB/Dialogs/DefaultLocationPropertiesEditControl.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using GB.Shared.GBMFile;
+using GB.Shared.Controls;
 
 namespace GB.GBMB.Dialogs
 {
@@ -19,7 +20,7 @@ namespace GB.GBMB.Dialogs
 
 		public GBMObjectMapProperties Properties {
 			get { return properties; }
-			set { properties = value; this.Invalidate(true); }
+			set { properties = value; RecreateTextBoxes(); this.Invalidate(true); }
 		}
 		public GBMObjectDefaultTilePropertyValues DefaultProperties {
 			get { return defaultProperties; }
@@ -28,6 +29,48 @@ namespace GB.GBMB.Dialogs
 
 		public DefaultLocationPropertiesEditControl() {
 			InitializeComponent();
+		}
+
+		private NumericTextBox[] textBoxes = new NumericTextBox[0];
+
+		private void RecreateTextBoxes() {
+			const int BOX_WIDTH = 62, BOX_HEIGHT = 19;
+			const int BOX_X = 135;
+
+			if (properties != null) {
+				foreach (Control c in textBoxes) { c.Dispose(); }
+
+				textBoxes = new NumericTextBox[properties.Master.PropCount];
+
+				this.SuspendLayout();
+
+				for (int i = 0; i < textBoxes.Length; i++) {
+					int y = (i + 1) * BOX_HEIGHT;
+
+					NumericTextBox t = new NumericTextBox();
+					
+					Panel panel = new Panel();
+					panel.Location = new Point(BOX_X, y);
+					panel.Size = new Size(BOX_WIDTH, BOX_HEIGHT);
+					panel.BorderStyle = BorderStyle.None;
+					panel.GotFocus += new EventHandler((s, a) => { t.Focus(); t.SelectAll(); });
+					panel.Click += new EventHandler((s, a) => { t.Focus(); t.SelectAll(); });
+					panel.Padding = new Padding(2, 2, 2, 2);
+					panel.Cursor = Cursors.IBeam;
+
+					t.Dock = DockStyle.Fill;
+					t.BorderStyle = BorderStyle.None;
+					t.Value = 0;
+					t.TabStop = false;
+
+					textBoxes[i] = t;
+
+					panel.Controls.Add(textBoxes[i]);
+					this.Controls.Add(panel);
+				}
+
+				this.ResumeLayout();
+			}
 		}
 
 		protected override void OnPaint(PaintEventArgs e) {
@@ -46,6 +89,7 @@ namespace GB.GBMB.Dialogs
 				int y = (i + 1) * BOX_HEIGHT;
 
 				DrawTextRect(e.Graphics, properties.Properties[i].Name, PROPERTY_X, y, PROPERTY_WIDTH, BOX_HEIGHT);
+				DrawEditRect(e.Graphics, VALUE_X, y, VALUE_WIDTH, BOX_HEIGHT);
 			}
 		}
 
@@ -65,6 +109,12 @@ namespace GB.GBMB.Dialogs
 			BorderPaint.DrawBorderClipped(g, InnerBorderRect, SystemColors.ControlDark, Border3DSide.Right | Border3DSide.Bottom);
 
 			g.DrawString(text, this.Font, SystemBrushes.ControlText, TextRect, format);
+		}
+
+		private void DrawEditRect(Graphics g, int x, int y, int width, int height) {
+			Color color = Color.FromArgb(192, 192, 192);
+
+			BorderPaint.DrawBorderFull(g, new Rectangle(x, y, width, height), color, Border3DSide.Right | Border3DSide.Bottom);
 		}
 	}
 }
