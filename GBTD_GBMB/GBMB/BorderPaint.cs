c68095ef46d8a85ee2bed88e3cffea31110a7d10
Pokechu22 <pokechu022@gmail.com>
2015-04-13 07:20:25 -0700

    Created a custom border painting class.
    This is more of a convineince thing.  No more abuse of ContrlPaint.

---
+++ GBTD_GBMB/GBMB/BorderPaint.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace GB.GBMB
+{
+	internal static class BorderPaint
+	{
+		/// <summary>
+		/// Draws a border.  The bottom and right sides have 1 pixel "Clipped", for the other border side.
+		/// Use this method if you are making a full border.
+		/// </summary>
+		public static void DrawBorderClipped(Graphics g, Rectangle rect, Color color, Border3DSide sides) {
+			using (Pen p = new Pen(color)) {
+				if (sides.HasFlag(Border3DSide.Top)) {
+					g.DrawLine(p, rect.Left, rect.Top, rect.Right - 1, rect.Top);
+				}
+				if (sides.HasFlag(Border3DSide.Left)) {
+					g.DrawLine(p, rect.Left, rect.Top, rect.Left, rect.Bottom - 1);
+				}
+				if (sides.HasFlag(Border3DSide.Bottom)) {
+					g.DrawLine(p, rect.Left + 1, rect.Bottom - 1, rect.Right - 1, rect.Bottom - 1);
+				}
+				if (sides.HasFlag(Border3DSide.Right)) {
+					g.DrawLine(p, rect.Right - 1, rect.Top + 1, rect.Right - 1, rect.Bottom - 1);
+				}
+			}
+		}
+
+		/// <summary>
+		/// Draws a border all the way around.
+		/// </summary>
+		public static void DrawBorderFull(Graphics g, Rectangle rect, Color color, Border3DSide sides) {
+			using (Pen p = new Pen(color)) {
+				if (sides.HasFlag(Border3DSide.Top)) {
+					g.DrawLine(p, rect.Left, rect.Top, rect.Right - 1, rect.Top);
+				}
+				if (sides.HasFlag(Border3DSide.Left)) {
+					g.DrawLine(p, rect.Left, rect.Top, rect.Left, rect.Bottom - 1);
+				}
+				if (sides.HasFlag(Border3DSide.Bottom)) {
+					g.DrawLine(p, rect.Left, rect.Bottom - 1, rect.Right - 1, rect.Bottom - 1);
+				}
+				if (sides.HasFlag(Border3DSide.Right)) {
+					g.DrawLine(p, rect.Right - 1, rect.Top, rect.Right - 1, rect.Bottom - 1);
+				}
+			}
+		}
+	}
+}
