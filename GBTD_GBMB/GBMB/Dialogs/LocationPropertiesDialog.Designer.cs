43206ea61146b8a24d3aec1ea7dec27d794addc2
Pokechu22 <pokechu022@gmail.com>
2015-04-14 07:28:06 -0700

    Created the layout for "Property colors".

--- GBTD_GBMB/GBMB/Dialogs/LocationPropertiesDialog.Designer.cs
+++ GBTD_GBMB/GBMB/Dialogs/LocationPropertiesDialog.Designer.cs
@@ -25,11 +25,22 @@
 		/// the contents of this method with the code editor.
 		/// </summary>
 		private void InitializeComponent() {
+			System.Drawing.StringFormat stringFormat4 = new System.Drawing.StringFormat();
+			System.Drawing.StringFormat stringFormat3 = new System.Drawing.StringFormat();
 			this.okButton = new System.Windows.Forms.Button();
 			this.cancelButton = new System.Windows.Forms.Button();
 			this.helpButton = new System.Windows.Forms.Button();
 			this.propColorsGroupBox = new GB.Shared.Controls.GroupBox();
 			this.propertiesGroupBox = new GB.Shared.Controls.GroupBox();
+			this.redLabel = new GB.Shared.Controls.CleanLabel();
+			this.greenLabel = new GB.Shared.Controls.CleanLabel();
+			this.redPropertyComboBox = new System.Windows.Forms.ComboBox();
+			this.greenPropertyComboBox = new System.Windows.Forms.ComboBox();
+			this.redOperatorComboBox = new System.Windows.Forms.ComboBox();
+			this.greenOperatorComboBox = new System.Windows.Forms.ComboBox();
+			this.redOperandTextBox = new GB.Shared.Controls.NumericTextBox();
+			this.greenOperandTextBox = new GB.Shared.Controls.NumericTextBox();
+			this.propColorsGroupBox.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// okButton
@@ -62,6 +73,14 @@
 			// 
 			// propColorsGroupBox
 			// 
+			this.propColorsGroupBox.Controls.Add(this.greenOperandTextBox);
+			this.propColorsGroupBox.Controls.Add(this.redOperandTextBox);
+			this.propColorsGroupBox.Controls.Add(this.greenOperatorComboBox);
+			this.propColorsGroupBox.Controls.Add(this.redOperatorComboBox);
+			this.propColorsGroupBox.Controls.Add(this.greenPropertyComboBox);
+			this.propColorsGroupBox.Controls.Add(this.redPropertyComboBox);
+			this.propColorsGroupBox.Controls.Add(this.greenLabel);
+			this.propColorsGroupBox.Controls.Add(this.redLabel);
 			this.propColorsGroupBox.Location = new System.Drawing.Point(8, 239);
 			this.propColorsGroupBox.Name = "propColorsGroupBox";
 			this.propColorsGroupBox.Size = new System.Drawing.Size(273, 77);
@@ -75,6 +94,100 @@
 			this.propertiesGroupBox.Size = new System.Drawing.Size(273, 226);
 			this.propertiesGroupBox.TabIndex = 3;
 			this.propertiesGroupBox.Text = "Properties";
+			// 
+			// redLabel
+			// 
+			this.redLabel.DisabledBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+			this.redLabel.DisabledForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+			stringFormat4.Alignment = System.Drawing.StringAlignment.Near;
+			stringFormat4.HotkeyPrefix = System.Drawing.Text.HotkeyPrefix.Show;
+			stringFormat4.LineAlignment = System.Drawing.StringAlignment.Near;
+			stringFormat4.Trimming = System.Drawing.StringTrimming.Character;
+			this.redLabel.Format = stringFormat4;
+			this.redLabel.Location = new System.Drawing.Point(14, 23);
+			this.redLabel.Name = "redLabel";
+			this.redLabel.Size = new System.Drawing.Size(25, 14);
+			this.redLabel.TabIndex = 4;
+			this.redLabel.TabStop = false;
+			this.redLabel.Text = "&Red";
+			// 
+			// greenLabel
+			// 
+			this.greenLabel.DisabledBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+			this.greenLabel.DisabledForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+			stringFormat3.Alignment = System.Drawing.StringAlignment.Near;
+			stringFormat3.HotkeyPrefix = System.Drawing.Text.HotkeyPrefix.Show;
+			stringFormat3.LineAlignment = System.Drawing.StringAlignment.Near;
+			stringFormat3.Trimming = System.Drawing.StringTrimming.Character;
+			this.greenLabel.Format = stringFormat3;
+			this.greenLabel.Location = new System.Drawing.Point(14, 47);
+			this.greenLabel.Name = "greenLabel";
+			this.greenLabel.Size = new System.Drawing.Size(36, 14);
+			this.greenLabel.TabIndex = 5;
+			this.greenLabel.TabStop = false;
+			this.greenLabel.Text = "&Green";
+			// 
+			// redPropertyComboBox
+			// 
+			this.redPropertyComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.redPropertyComboBox.FormattingEnabled = true;
+			this.redPropertyComboBox.Items.AddRange(new object[] {
+            "Actual properties",
+            "Will appear here",
+            "You should never",
+            "See this"});
+			this.redPropertyComboBox.Location = new System.Drawing.Point(64, 20);
+			this.redPropertyComboBox.Name = "redPropertyComboBox";
+			this.redPropertyComboBox.Size = new System.Drawing.Size(105, 21);
+			this.redPropertyComboBox.TabIndex = 6;
+			// 
+			// greenPropertyComboBox
+			// 
+			this.greenPropertyComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.greenPropertyComboBox.FormattingEnabled = true;
+			this.greenPropertyComboBox.Items.AddRange(new object[] {
+            "Actual properties",
+            "Will appear here",
+            "You should never",
+            "See this"});
+			this.greenPropertyComboBox.Location = new System.Drawing.Point(64, 44);
+			this.greenPropertyComboBox.Name = "greenPropertyComboBox";
+			this.greenPropertyComboBox.Size = new System.Drawing.Size(105, 21);
+			this.greenPropertyComboBox.TabIndex = 7;
+			// 
+			// redOperatorComboBox
+			// 
+			this.redOperatorComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.redOperatorComboBox.FormattingEnabled = true;
+			this.redOperatorComboBox.Location = new System.Drawing.Point(176, 20);
+			this.redOperatorComboBox.Name = "redOperatorComboBox";
+			this.redOperatorComboBox.Size = new System.Drawing.Size(38, 21);
+			this.redOperatorComboBox.TabIndex = 8;
+			// 
+			// greenOperatorComboBox
+			// 
+			this.greenOperatorComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.greenOperatorComboBox.FormattingEnabled = true;
+			this.greenOperatorComboBox.Location = new System.Drawing.Point(176, 44);
+			this.greenOperatorComboBox.Name = "greenOperatorComboBox";
+			this.greenOperatorComboBox.Size = new System.Drawing.Size(38, 21);
+			this.greenOperatorComboBox.TabIndex = 9;
+			// 
+			// redOperandTextBox
+			// 
+			this.redOperandTextBox.Location = new System.Drawing.Point(221, 20);
+			this.redOperandTextBox.Name = "redOperandTextBox";
+			this.redOperandTextBox.Size = new System.Drawing.Size(38, 21);
+			this.redOperandTextBox.TabIndex = 10;
+			this.redOperandTextBox.Value = ((uint)(0u));
+			// 
+			// greenOperandTextBox
+			// 
+			this.greenOperandTextBox.Location = new System.Drawing.Point(221, 44);
+			this.greenOperandTextBox.Name = "greenOperandTextBox";
+			this.greenOperandTextBox.Size = new System.Drawing.Size(38, 21);
+			this.greenOperandTextBox.TabIndex = 11;
+			this.greenOperandTextBox.Value = ((uint)(0u));
 			// 
 			// LocationPropertiesDialog
 			// 
@@ -93,6 +206,7 @@
 			this.MinimizeBox = false;
 			this.Name = "LocationPropertiesDialog";
 			this.Text = "Location properties";
+			this.propColorsGroupBox.ResumeLayout(false);
 			this.ResumeLayout(false);
 
 		}
@@ -104,5 +218,13 @@
 		private System.Windows.Forms.Button helpButton;
 		private Shared.Controls.GroupBox propertiesGroupBox;
 		private Shared.Controls.GroupBox propColorsGroupBox;
+		private Shared.Controls.NumericTextBox greenOperandTextBox;
+		private Shared.Controls.NumericTextBox redOperandTextBox;
+		private System.Windows.Forms.ComboBox greenOperatorComboBox;
+		private System.Windows.Forms.ComboBox redOperatorComboBox;
+		private System.Windows.Forms.ComboBox greenPropertyComboBox;
+		private System.Windows.Forms.ComboBox redPropertyComboBox;
+		private Shared.Controls.CleanLabel greenLabel;
+		private Shared.Controls.CleanLabel redLabel;
 	}
 }
