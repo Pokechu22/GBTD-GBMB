21756e74e251708dc12efda783bc848f80787a97
Pokechu22 <pokechu022@gmail.com>
2015-04-17 09:07:30 -0700

    Added BorderPaint methods that use x, y, width, height rather than rect.

--- GBTD_GBMB/GBMB/BorderPaint.cs
+++ GBTD_GBMB/GBMB/BorderPaint.cs
@@ -13,6 +13,14 @@ namespace GB.GBMB
 		/// Draws a border.  The bottom and right sides have 1 pixel "Clipped", for the other border side.
 		/// Use this method if you are making a full border.
 		/// </summary>
+		public static void DrawBorderClipped(Graphics g, int x, int y, int width, int height, Color color, Border3DSide sides) {
+			DrawBorderClipped(g, new Rectangle(x, y, width, height), color, sides);
+		}
+
+		/// <summary>
+		/// Draws a border.  The bottom and right sides have 1 pixel "Clipped", for the other border side.
+		/// Use this method if you are making a full border.
+		/// </summary>
 		public static void DrawBorderClipped(Graphics g, Rectangle rect, Color color, Border3DSide sides) {
 			using (Pen p = new Pen(color)) {
 				if (sides.HasFlag(Border3DSide.Top)) {
@@ -28,6 +36,13 @@ namespace GB.GBMB
 					g.DrawLine(p, rect.Right - 1, rect.Top + 1, rect.Right - 1, rect.Bottom - 1);
 				}
 			}
+		}
+
+		/// <summary>
+		/// Draws a border all the way around.
+		/// </summary>
+		public static void DrawBorderFull(Graphics g, int x, int y, int width, int height, Color color, Border3DSide sides) {
+			DrawBorderFull(g, new Rectangle(x, y, width, height), color, sides);
 		}
 
 		/// <summary>
