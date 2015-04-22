e5a43a307471f53772e9f7748e5bf62b0b271722
Pokechu22 <pokechu022@gmail.com>
2015-04-22 12:41:29 -0700

    Created "WriteSizeDefines" method.

---
+++ GBTD_GBMB/GBMB/Exporting/GBDKCMapExporter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GB.Shared.GBMFile;
+
+namespace GB.GBMB.Exporting
+{
+	/// <summary>
+	/// An exporter that exports to GBDK C.
+	/// </summary>
+	public class GBDKCMapExporter : MapExportTextBase
+	{
+		protected override string HeaderBegin {
+			get { return "/*" + Stream.NewLine; }
+		}
+
+		protected override string HeaderLine {
+			get { return ""; }
+		}
+
+		protected override string HeaderEnd {
+			get { return Stream.NewLine + "*/"; }
+		}
+
+		public override void WriteSizeDefines(GBMObjectMapExportSettings settings) {
+			Stream.WriteLine("#define {0}Width {1}", settings.LabelName.Trim(), settings.Master.Width);
+			Stream.WriteLine("#define {0}Height {1}", settings.LabelName.Trim(), settings.Master.Height);
+			Stream.WriteLine("#define {0}Bank {1}", settings.LabelName.Trim(), settings.Bank);
+		}
+	}
+}
