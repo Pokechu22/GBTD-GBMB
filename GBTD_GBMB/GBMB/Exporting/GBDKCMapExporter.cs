dadf767b40d7e0e188d6591c38e1b8c551c1935d
Pokechu22 <pokechu022@gmail.com>
2015-04-24 07:21:49 -0700

    Implemented the aforementioned methods.

--- GBTD_GBMB/GBMB/Exporting/GBDKCMapExporter.cs
+++ GBTD_GBMB/GBMB/Exporting/GBDKCMapExporter.cs
@@ -23,6 +23,14 @@ namespace GB.GBMB.Exporting
 			get { return Stream.NewLine + "*/"; }
 		}
 
+		protected override string BlockBegin {
+			get { return "{" + Stream.NewLine; }
+		}
+
+		protected override string BlockEnd {
+			get { return "};" + Stream.NewLine; }
+		}
+
 		public override void WriteSizeDefines(GBMObjectMapExportSettings settings) {
 			Stream.WriteLine("#define {0}Width {1}", settings.LabelName.Trim(), settings.Master.Width);
 			Stream.WriteLine("#define {0}Height {1}", settings.LabelName.Trim(), settings.Master.Height);
@@ -48,6 +56,27 @@ namespace GB.GBMB.Exporting
 				}
 			} else {
 				Stream.WriteLine("unsigned char {0}PLN{1}[] =", name, plane);
+			}
+		}
+
+		public override void WriteDataLine(byte[] bytes, ref int position, int count) {
+			int endPos = position + count;
+
+			Stream.Write("  ");
+			while (position < endPos && position < bytes.Length) {
+				Stream.Write("0x{0:x2}", bytes[position]);
+
+				position++;
+
+				if (position < endPos || position < bytes.Length) {
+					Stream.Write(",");
+				}
+			}
+
+			if (position != bytes.Length) {
+				Stream.WriteLine(",");
+			} else {
+				Stream.WriteLine();
 			}
 		}
 	}
