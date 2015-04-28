ebda8e246fca0d2e0ac900da60af3748bd24d37e
Pokechu22 <pokechu022@gmail.com>
2015-04-28 11:56:11 -0700

    Set up the "WriteSizeDefines" method for ASM.

--- GBTD_GBMB/GBMB/Exporting/ASMExportBase.cs
+++ GBTD_GBMB/GBMB/Exporting/ASMExportBase.cs
@@ -2,11 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using GB.Shared.GBMFile;
+using System.IO;
+using GB.Shared.GBRFile;
 
 namespace GB.GBMB.Exporting
 {
 	public abstract class ASMExportBase : MapExportTextBase
 	{
+		/// <summary>
+		/// The text used in the SizeDefines section between the term and the value.
+		/// 
+		/// EG RGBDS uses "<c>EQU</c>".  Do not include spaces.
+		/// </summary>
+		protected abstract string DefineEQU { get; }
+
 		protected override string HeaderBegin {
 			get { return ""; }
 		}
@@ -25,6 +35,12 @@ namespace GB.GBMB.Exporting
 
 		protected override string FooterEnd {
 			get { return ""; }
+		}
+
+		public override void WriteSizeDefines(GBMObjectMapExportSettings settings) {
+			Stream.WriteLine("{0}Width  {1} {2}", settings.LabelName.Trim(), DefineEQU, settings.Master.Width);
+			Stream.WriteLine("{0}Height {1} {2}", settings.LabelName.Trim(), DefineEQU, settings.Master.Height);
+			Stream.WriteLine("{0}Bank   {1} {2}", settings.LabelName.Trim(), DefineEQU, settings.Bank);
 		}
 	}
 }
