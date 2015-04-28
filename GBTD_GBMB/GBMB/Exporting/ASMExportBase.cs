30a81ef65419b3a0f900d8f8d20d0cf1f0e6d41e
Pokechu22 <pokechu022@gmail.com>
2015-04-28 11:50:54 -0700

    Started ASM export with abstract class ASMExportBase.
    Each of the ASM exports has similarities.  So I'll start with this and
    expand it as I go.

---
+++ GBTD_GBMB/GBMB/Exporting/ASMExportBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GB.GBMB.Exporting
+{
+	public abstract class ASMExportBase : MapExportTextBase
+	{
+		protected override string HeaderBegin {
+			get { return ""; }
+		}
+
+		protected override string HeaderLine {
+			get { return ";"; }
+		}
+
+		protected override string HeaderEnd {
+			get { return ""; }
+		}
+
+		protected override string FooterBegin {
+			get { return ";"; }
+		}
+
+		protected override string FooterEnd {
+			get { return ""; }
+		}
+	}
+}
