c533123669fa8212934caeb5d7ff57087b3dd27f
Pokechu22 <pokechu022@gmail.com>
2015-06-10 12:13:25 -0700

    Added the inner controls to ColorPicker.

--- GBTD_GBMB/GBTD/Dialogs/ColorPicker.Designer.cs
+++ GBTD_GBMB/GBTD/Dialogs/ColorPicker.Designer.cs
@@ -27,12 +27,23 @@
 		private void InitializeComponent() {
 			GB.Shared.Controls.Border mainBorder1;
 			GB.Shared.Controls.Border mainBorder2;
+			System.Drawing.StringFormat stringFormat4 = new System.Drawing.StringFormat();
+			System.Drawing.StringFormat stringFormat5 = new System.Drawing.StringFormat();
+			System.Drawing.StringFormat stringFormat6 = new System.Drawing.StringFormat();
 			this.pictureBorder = new GB.Shared.Controls.Border();
 			this.rgbBorder = new GB.Shared.Controls.Border();
 			this.actualColor = new GB.Shared.Controls.Border();
 			this.hoveredColor = new GB.Shared.Controls.Border();
+			this.pictureBox = new System.Windows.Forms.PictureBox();
+			this.rLabel = new GB.Shared.Controls.CleanLabel();
+			this.gLabel = new GB.Shared.Controls.CleanLabel();
+			this.bLabel = new GB.Shared.Controls.CleanLabel();
+			this.redTextBox = new GB.Shared.Controls.NumericTextBox();
+			this.greenTextBox = new GB.Shared.Controls.NumericTextBox();
+			this.blueTextBox = new GB.Shared.Controls.NumericTextBox();
 			mainBorder1 = new GB.Shared.Controls.Border();
 			mainBorder2 = new GB.Shared.Controls.Border();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// mainBorder1
@@ -102,6 +113,7 @@
 			// 
 			// actualColor
 			// 
+			this.actualColor.BackColor = System.Drawing.Color.White;
 			this.actualColor.BottomBorder = System.Windows.Forms.Border3DStyle.SunkenOuter;
 			this.actualColor.DrawOrder = new System.Windows.Forms.Border3DSide[] {
         System.Windows.Forms.Border3DSide.Top,
@@ -118,6 +130,7 @@
 			// 
 			// hoveredColor
 			// 
+			this.hoveredColor.BackColor = System.Drawing.Color.White;
 			this.hoveredColor.BottomBorder = System.Windows.Forms.Border3DStyle.SunkenOuter;
 			this.hoveredColor.DrawOrder = new System.Windows.Forms.Border3DSide[] {
         System.Windows.Forms.Border3DSide.Top,
@@ -132,10 +145,107 @@
 			this.hoveredColor.TabIndex = 6;
 			this.hoveredColor.TopBorder = System.Windows.Forms.Border3DStyle.SunkenOuter;
 			// 
+			// pictureBox
+			// 
+			this.pictureBox.Image = global::GB.GBTD.Properties.Resources.GAMMA;
+			this.pictureBox.Location = new System.Drawing.Point(4, 4);
+			this.pictureBox.Name = "pictureBox";
+			this.pictureBox.Size = new System.Drawing.Size(45, 96);
+			this.pictureBox.TabIndex = 7;
+			this.pictureBox.TabStop = false;
+			// 
+			// rLabel
+			// 
+			this.rLabel.DisabledBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+			this.rLabel.DisabledForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+			this.rLabel.Font = new System.Drawing.Font("Times New Roman", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			stringFormat4.Alignment = System.Drawing.StringAlignment.Near;
+			stringFormat4.HotkeyPrefix = System.Drawing.Text.HotkeyPrefix.Show;
+			stringFormat4.LineAlignment = System.Drawing.StringAlignment.Near;
+			stringFormat4.Trimming = System.Drawing.StringTrimming.Character;
+			this.rLabel.Format = stringFormat4;
+			this.rLabel.Location = new System.Drawing.Point(7, 141);
+			this.rLabel.Name = "rLabel";
+			this.rLabel.Size = new System.Drawing.Size(12, 14);
+			this.rLabel.TabIndex = 8;
+			this.rLabel.TabStop = false;
+			this.rLabel.Text = "&R";
+			// 
+			// gLabel
+			// 
+			this.gLabel.DisabledBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+			this.gLabel.DisabledForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+			this.gLabel.Font = new System.Drawing.Font("Times New Roman", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			stringFormat5.Alignment = System.Drawing.StringAlignment.Near;
+			stringFormat5.HotkeyPrefix = System.Drawing.Text.HotkeyPrefix.Show;
+			stringFormat5.LineAlignment = System.Drawing.StringAlignment.Near;
+			stringFormat5.Trimming = System.Drawing.StringTrimming.Character;
+			this.gLabel.Format = stringFormat5;
+			this.gLabel.Location = new System.Drawing.Point(7, 159);
+			this.gLabel.Name = "gLabel";
+			this.gLabel.Size = new System.Drawing.Size(13, 14);
+			this.gLabel.TabIndex = 9;
+			this.gLabel.TabStop = false;
+			this.gLabel.Text = "&G";
+			// 
+			// bLabel
+			// 
+			this.bLabel.DisabledBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+			this.bLabel.DisabledForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+			this.bLabel.Font = new System.Drawing.Font("Times New Roman", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			stringFormat6.Alignment = System.Drawing.StringAlignment.Near;
+			stringFormat6.HotkeyPrefix = System.Drawing.Text.HotkeyPrefix.Show;
+			stringFormat6.LineAlignment = System.Drawing.StringAlignment.Near;
+			stringFormat6.Trimming = System.Drawing.StringTrimming.Character;
+			this.bLabel.Format = stringFormat6;
+			this.bLabel.Location = new System.Drawing.Point(7, 177);
+			this.bLabel.Name = "bLabel";
+			this.bLabel.Size = new System.Drawing.Size(12, 14);
+			this.bLabel.TabIndex = 10;
+			this.bLabel.TabStop = false;
+			this.bLabel.Text = "&B";
+			// 
+			// redTextBox
+			// 
+			this.redTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.redTextBox.Location = new System.Drawing.Point(26, 139);
+			this.redTextBox.Name = "redTextBox";
+			this.redTextBox.Size = new System.Drawing.Size(18, 17);
+			this.redTextBox.TabIndex = 11;
+			this.redTextBox.Text = "31";
+			this.redTextBox.Value = ((uint)(31u));
+			// 
+			// greenTextBox
+			// 
+			this.greenTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.greenTextBox.Location = new System.Drawing.Point(26, 157);
+			this.greenTextBox.Name = "greenTextBox";
+			this.greenTextBox.Size = new System.Drawing.Size(18, 17);
+			this.greenTextBox.TabIndex = 12;
+			this.greenTextBox.Text = "31";
+			this.greenTextBox.Value = ((uint)(31u));
+			// 
+			// blueTextBox
+			// 
+			this.blueTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.blueTextBox.Location = new System.Drawing.Point(26, 175);
+			this.blueTextBox.Name = "blueTextBox";
+			this.blueTextBox.Size = new System.Drawing.Size(18, 17);
+			this.blueTextBox.TabIndex = 13;
+			this.blueTextBox.Text = "31";
+			this.blueTextBox.Value = ((uint)(31u));
+			// 
 			// ColorPicker
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.blueTextBox);
+			this.Controls.Add(this.greenTextBox);
+			this.Controls.Add(this.redTextBox);
+			this.Controls.Add(this.bLabel);
+			this.Controls.Add(this.gLabel);
+			this.Controls.Add(this.rLabel);
+			this.Controls.Add(this.pictureBox);
 			this.Controls.Add(this.hoveredColor);
 			this.Controls.Add(this.actualColor);
 			this.Controls.Add(this.rgbBorder);
@@ -144,6 +254,7 @@
 			this.Controls.Add(mainBorder1);
 			this.Name = "ColorPicker";
 			this.Size = new System.Drawing.Size(53, 230);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
 			this.ResumeLayout(false);
 
 		}
@@ -154,6 +265,13 @@
 		private Shared.Controls.Border rgbBorder;
 		private Shared.Controls.Border actualColor;
 		private Shared.Controls.Border hoveredColor;
+		private System.Windows.Forms.PictureBox pictureBox;
+		private Shared.Controls.CleanLabel rLabel;
+		private Shared.Controls.CleanLabel gLabel;
+		private Shared.Controls.CleanLabel bLabel;
+		private Shared.Controls.NumericTextBox redTextBox;
+		private Shared.Controls.NumericTextBox greenTextBox;
+		private Shared.Controls.NumericTextBox blueTextBox;
 
 	}
 }
