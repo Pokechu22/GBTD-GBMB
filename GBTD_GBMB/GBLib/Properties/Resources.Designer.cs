902256dd7291daef1af4694f7f6a93cb501a6666
Pokechu22 <pokechu022@gmail.com>
2015-06-10 10:26:07 -0700

    Added Gamma and GBCGamma to GBTD.
    (Also, removed the unused images in GBComp)

--- GBTD_GBMB/GBLib/Properties/Resources.Designer.cs
+++ GBTD_GBMB/GBLib/Properties/Resources.Designer.cs
@@ -59,25 +59,5 @@ namespace GB.Shared.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap GAMMA {
-            get {
-                object obj = ResourceManager.GetObject("GAMMA", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap GBCGAMMA {
-            get {
-                object obj = ResourceManager.GetObject("GBCGAMMA", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
--- GBTD_GBMB/GBTD/Properties/Resources.Designer.cs
+++ GBTD_GBMB/GBTD/Properties/Resources.Designer.cs
@@ -203,6 +203,26 @@ namespace GB.GBTD.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap GAMMA {
+            get {
+                object obj = ResourceManager.GetObject("GAMMA", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap GBCGAMMA {
+            get {
+                object obj = ResourceManager.GetObject("GBCGAMMA", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap GBTD_Image {
             get {
                 object obj = ResourceManager.GetObject("GBTD_Image", resourceCulture);
